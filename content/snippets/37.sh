b2c54397b8d00167c5c42510ab163e067d25e1e4
Greg Hurrell <greg@hurrell.net>
2011-05-23 03:29:19 +0000

    Import snippet: Coffee shop internet

---
+++ content/snippets/37.sh
@@ -0,0 +1,27 @@
+---
+title: Coffee shop internet
+tags: starbucks
+---
+$ ping wincent.com
+PING wincent.com (184.73.232.208): 56 data bytes
+Request timeout for icmp_seq 0
+Request timeout for icmp_seq 1
+64 bytes from 184.73.232.208: icmp_seq=0 ttl=46 time=2142.175 ms
+64 bytes from 184.73.232.208: icmp_seq=1 ttl=46 time=1155.098 ms
+64 bytes from 184.73.232.208: icmp_seq=2 ttl=46 time=520.864 ms
+64 bytes from 184.73.232.208: icmp_seq=3 ttl=46 time=899.595 ms
+64 bytes from 184.73.232.208: icmp_seq=4 ttl=46 time=610.829 ms
+Request timeout for icmp_seq 7
+64 bytes from 184.73.232.208: icmp_seq=6 ttl=46 time=2760.562 ms
+64 bytes from 184.73.232.208: icmp_seq=7 ttl=46 time=1810.635 ms
+64 bytes from 184.73.232.208: icmp_seq=8 ttl=46 time=810.589 ms
+64 bytes from 184.73.232.208: icmp_seq=9 ttl=46 time=459.481 ms
+Request timeout for icmp_seq 12
+64 bytes from 184.73.232.208: icmp_seq=10 ttl=46 time=3175.602 ms
+64 bytes from 184.73.232.208: icmp_seq=11 ttl=46 time=2177.557 ms
+64 bytes from 184.73.232.208: icmp_seq=12 ttl=46 time=1699.033 ms
+64 bytes from 184.73.232.208: icmp_seq=13 ttl=46 time=698.401 ms
+^C
+--- wincent.com ping statistics ---
+16 packets transmitted, 13 packets received, 18.8% packet loss
+round-trip min/avg/max/stddev = 459.481/1455.417/3175.602/868.584 ms
