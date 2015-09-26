37fc6ce23052f0d268a2f5e3e8845ee47710fa7c
Greg Hurrell <greg@hurrell.net>
2015-09-25 22:41:47 -0700

    Demo main server talking to wikiserve

--- server/server.js
+++ server/server.js
@@ -1,8 +1,36 @@
 var express = require('express');
+var http = require('http');
+
 var app = express();
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  var wikitext = http.request(
+    {
+      hostname: 'localhost',
+      port: 8080,
+      path: '/wikitext',
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-Wikitext-Corpus-Digest': 'foo',
+      },
+    },
+    function(r) {
+      r.setEncoding('utf8');
+      r.on('data', function(chunk) {
+        console.log('BODY: ' + chunk);
+        response.send(chunk);
+      });
+      r.on('end', function() {
+        console.log('No more data in response.')
+      })
+    }
+  );
+  wikitext.write(JSON.stringify([
+    {wikitext: '= What ='},
+  ]));
+  wikitext.end();
+
 });
 
 var server = app.listen(3000, function() {
