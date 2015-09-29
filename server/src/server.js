e7f6fd4989640073c27b26940c9020e0aba0b93b
Greg Hurrell <greg@hurrell.net>
2015-09-28 18:07:59 -0700

    Use express-graphql

--- server/src/server.js
+++ server/src/server.js
@@ -2,18 +2,20 @@
 
 import Promise from 'bluebird';
 import express from 'express';
+import graphqlHTTP from 'express-graphql';
 
 // Non-Relay demo.
-import {graphql} from 'graphql';
 import schema from './schema';
 
 const app = express();
 
+// curl /
 app.get('/', (request, response) => {
-  const query = '{ hello }';
-  graphql(schema, query)
-    .then(result => response.send(JSON.stringify(result)));
+  response.send('root');
 });
+
+// curl 'localhost:3000/graphql?query=query+Query\{hello\}'
+app.use('/graphql', graphqlHTTP(request => ({schema})));
 
 const server = app.listen(3000, () => {
   const host = server.address().address;
