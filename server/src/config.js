94c33831f18d6ea154e6566144d66d09c2a00270
Greg Hurrell <greg@hurrell.net>
2015-09-28 17:42:25 -0700

    Show that we can connect to MySQL
    Obviously I'm going to rip this out, but just for now showing that I can
    connect to a database.

---
+++ server/src/config.js
@@ -0,0 +1,5 @@
+'use strict';
+
+const config = require('../config');
+
+export default config;
--- server/src/schema.js
+++ server/src/schema.js
@@ -1,11 +1,16 @@
 'use strict';
 
+import Promise from 'bluebird';
+import config from './config';
 import {
   GraphQLObjectType,
   GraphQLSchema,
   GraphQLString,
 } from 'graphql';
+import mysql from 'mysql';
 import wikify from './wikify';
+
+const {host, user, password} = config.db;
 
 const schema = new GraphQLSchema({
   query: new GraphQLObjectType({
@@ -14,6 +19,29 @@ const schema = new GraphQLSchema({
       hello: {
         type: GraphQLString,
         resolve() {
+          // Demo only.
+          const connection = mysql.createConnection({host, user, password});
+          new Promise((resolve, reject) => {
+            connection.connect(error => {
+              if (error) {
+                reject(error);
+              } else {
+                resolve(connection);
+              }
+            });
+          }).then((connection, reject) => {
+            connection.query('SELECT 1', (error, rows) => {
+              if (error) {
+                reject(error);
+              } else {
+                console.log(
+                  'Doing nothing with this result, just proving I could get it',
+                  rows
+                );
+                return rows;
+              }
+            });
+          });
           return wikify([{wikitext: "'''world'''"}]);
         }
       }
