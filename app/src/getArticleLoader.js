17afb70d1481f0f2e78711c26489814a68d824a7
Greg Hurrell <greg@hurrell.net>
2015-10-03 16:22:28 -0700

    Shell out to Git for rev-list operation
    As it is about 100x faster. Still not instant, so I will want to add a
    memcached layer.

--- app/src/getArticleLoader.js
+++ app/src/getArticleLoader.js
@@ -8,7 +8,9 @@ import Promise from 'bluebird';
 import DataLoader from 'dataloader';
 import nodegit from 'nodegit';
 import path from 'path';
+import process from 'process';
 import {Extensions} from './Markup';
+import git from './git';
 
 async function loadArticle(key): Promise {
   const repo = await nodegit.Repository.open(path.resolve(__dirname, '../../.git'));
@@ -37,50 +39,35 @@ async function loadArticle(key): Promise {
 
   const blob = await treeEntry.getBlob();
 
-  // Commit#history is dead slow, and so is Revwalk.
-  // TODO: try it using dumb callbacks, or promises
-  let last = null;
-  let earliest = null;
-  const revwalk = nodegit.Revwalk.create(repo);
-  revwalk.push(head.id());
-
   // This is committer time, which is appropriate for articles (where recency of
   // update matters). For posts, creation order matters, so we'll go with
   // topological search (which workes because I imported old content in-order)
   // and we'll get creation date from the author date.
-  revwalk.sorting(nodegit.Revwalk.SORT.TIME);
-
-  let current = head;
-  while (current) {
-    const diffList = await current.getDiff();
-    for (let i = 0; i < diffList.length; i++) {
-      const found = (await diffList[i].patches()).find(patch => {
-        return (
-          patch.oldFile().path() === 'content/wiki/' + key + '.' + extension ||
-          patch.newFile().path() === 'content/wiki/' + key + '.' + extension
-        );
-      });
-      if (found) {
-        earliest = current;
-        if (!last) {
-          last = current;
-        }
-        break;
-      }
-    }
-    const sha = await revwalk.next();
-    if (!sha) {
-      break;
-    }
-    current = await repo.getCommit(sha);
-  }
+  const revs = await git(
+    'rev-list',
+    '--date-order',
+    'content',
+    '--',
+    path.relative(
+      path.resolve(process.cwd()),
+      path.resolve(__dirname, '..', '..', 'content', 'wiki', `${key}.${extension}`),
+    ),
+  );
+  const mostRecent = revs.slice(0, 40);
+  const oldest = revs.trim().slice(-40);
 
   return Promise.resolve({
     id: key,
     title: key,
     body: blob.toString(),
-    createdAt: new Date(earliest.author().when().time() * 1000), // Author date of earliest.
-    updatedAt: last.date(), // Commit date of latest.
+    createdAt: oldest ?
+      // Author date of earliest.
+      new Date((await repo.getCommit(oldest)).author().when().time() * 1000) :
+      null,
+    updatedAt: mostRecent ?
+      // Commit date of latest.
+      (await repo.getCommit(mostRecent)).date() :
+      null,
   });
 }
 
---
+++ app/src/git.js
@@ -0,0 +1,41 @@
+/**
+ * @flow
+ */
+
+'use strict';
+
+import type Promise from 'bluebird';
+import {spawn} from 'child_process';
+
+// TODO: expose streamy version of this
+function run(command, ...args: Array<string>): Promise<string> {
+  const promise = new Promise((resolve, reject) => {
+    const child = spawn(command, args);
+    let stdout = '';
+
+    child.stdout.on('data', data => stdout += data);
+
+    child.on('error', error => {
+      if (promise.isPending()) {
+        reject(error);
+      }
+    });
+
+    child.on('close', code => {
+      if (code) {
+        if (promise.isPending()) {
+          reject(code);
+        }
+      } else {
+        resolve(stdout);
+      }
+    });
+
+  });
+
+  return promise;
+}
+
+export default function git(...args: Array<string>): Promise {
+  return run('git', ...args);
+}
