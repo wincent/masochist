470ee31b7503617bf923510935589d499502ec7e
Greg Hurrell <greg@hurrell.net>
2015-10-04 17:11:20 -0700

    Add cache breaking functionality
    And use it to default to resetting the cache on app
    start-up in __DEV__ mode.

--- app/src/server/Cache.js
+++ app/src/server/Cache.js
@@ -8,10 +8,33 @@ import Memcached from 'memcached';
 
 const memcached = new Memcached('localhost:11211');
 
+/**
+ * Can manually force cache invalidation by bumping this.
+ *
+ * May one day consider using deployed hash instead.
+ */
+const CACHE_VERSION = '1';
+
+/**
+ * In __DEV__ we invalidate the cache automatically on boot (ie. when this
+ * module is first evaluated), by using this time-dependent seed value.
+ */
+const STARTUP_SEED = Date.now().toString(36);
+
+function getBreaker(): string {
+  if (__DEV__) {
+    return STARTUP_SEED;
+  } else {
+    return CACHE_VERSION;
+  }
+}
+
 // TODO: degrade more gracefully if memcached goes down (currently we just wait
 // forever(?))
 const Cache = {
   get(key: string, missCallback: Promise<value, Error>): Promise<value, Error> {
+    key = key + getBreaker();
+
     return new Promise((resolve, reject) => {
       memcached.get(key, (error, data) => {
         if (error) {
