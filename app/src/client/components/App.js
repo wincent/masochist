24b88e5f53925ed7fdf19642a8bfc8b096fd26bc
Greg Hurrell <greg@hurrell.net>
2015-10-06 20:48:34 -0700

    Show some article metainfo on the index page
    Obviously this will get refactored into smaller components,
    but just proving that data fetching works for now.
    
    Pagination query is coming back empty, so I have to look into
    that as a follow-up. Probably did something wrong on the schema
    side.

--- app/src/client/components/App.js
+++ app/src/client/components/App.js
@@ -4,20 +4,72 @@ import Relay from 'react-relay';
 import './App.css';
 
 class App extends React.Component {
+  // TODO: swap this for autobind decorator
+  _handleSeeMore = () => {
+    this.props.relay.setVariables({
+      count: this.props.relay.variables.count + 10,
+    });
+  }
+
   render() {
     return (
       <div>
-        {this.props.viewer.name}
+        <h1>Hi, {this.props.viewer.name}</h1>
+        <ul>
+          {
+            this.props.viewer.articles.edges.map(edge => {
+              const {id, title, createdAt, updatedAt, tags} = edge.node;
+              return (
+                <li key={id}>
+                  <a href={'/wiki/' + title.replace(/ /g, '_')}>
+                    {title}
+                  </a>
+                  <ul>
+                    {
+                      tags.map((tag, i) => (
+                        <li key={i}>
+                          <a href={'/tags/' + tag}>{tag}</a>
+                        </li>
+                      ))
+                    }
+                  </ul>
+                </li>
+              );
+            })
+          }
+        </ul>
+        {
+          this.props.viewer.articles.pageInfo.hasNextPage ?
+            <a href="#more" onClick={this._handleSeeMore}>Load more&hellip;</a> :
+            null
+        }
       </div>
     );
   }
 }
 
 export default Relay.createContainer(App, {
+  initialVariables: {
+    count: 10,
+  },
   fragments: {
     viewer: () => Relay.QL`
       fragment on User {
         name
+        articles(first: $count) {
+          edges {
+            node {
+              id
+              title
+              createdAt
+              updatedAt
+              tags
+            }
+          }
+          pageInfo {
+            hasNextPage
+          }
+        }
       }
     `,
   },
