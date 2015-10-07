66481ec3b5ba8fd80f84452864218e740087973a
Greg Hurrell <greg@hurrell.net>
2015-10-07 10:47:25 -0700

    Clarify which license applies to which file
    At the moment, there is exactly one file here that uses the LICENSE and
    PATENTS grant from the upstream FB project, so make that explicit.

--- app/babel/devBabelPlugin.js
+++ app/babel/devBabelPlugin.js
@@ -3,8 +3,9 @@
  * All rights reserved.
  *
  * This source code is licensed under the BSD-style license found in the
- * LICENSE file in the root directory of this source tree. An additional grant
- * of patent rights can be found in the PATENTS file in the same directory.
+ * LICENSE-fbjs file in this file's directory in the source tree. An additional
+ * grant of patent rights can be found in the PATENTS-fbjs file in the same
+ * directory.
  */
 
 module.exports = function(babel) {
