3ca7de26d365105d5e74693aab0d5417b5d4511e
Greg Hurrell <greg@hurrell.net>
2015-10-13 08:16:56 -0700

    Add some links to the footer

--- app/src/client/components/Footer.js
+++ app/src/client/components/Footer.js
@@ -1,6 +1,19 @@
 import React from 'react';
+import {Link} from 'react-router';
 
 import './Footer.css';
+
+const FooterLink = ({target, text}) => (
+  <li>
+    <Link to={target}>{text}</Link>
+  </li>
+);
+
+const StaticFooterLink = ({target, text}) => (
+  <li>
+    <a href={target}>{text}</a>
+  </li>
+);
 
 export default class Footer extends React.Component {
   render() {
@@ -9,10 +22,19 @@ export default class Footer extends React.Component {
         <div className="container">
           <div className="row">
             <div className="four columns">
-              <h6>Column 1</h6>
+              <h6>Site</h6>
+              <ul>
+                <FooterLink target="/blog" text="Blog" />
+                <FooterLink target="/wiki" text="Wiki" />
+                <FooterLink target="/snippets" text="Snippets" />
+              </ul>
             </div>
             <div className="four columns">
-              <h6>Column 2</h6>
+              <h6>Legacy</h6>
+              <ul>
+                {/* TODO: copy the mirrors into /public in this repo, so I have everything in one place */}
+                <StaticFooterLink target="https://wincent.com/products" text="Products" />
+              </ul>
             </div>
             <div className="four columns">
               <h6>Contact</h6>
