7b1f46547ce4d62d234caa6ae53fb456f3d653d9
Greg Hurrell <greg@hurrell.net>
2015-10-13 23:45:30 -0700

    Move old "static" assets under "assets/static"
    So, we don't want these old assets overlapping with the new ones that I
    have under "static/". Let's move them under "assets/static/",
    effectively making all the Rails-app stuff reside under "assets/" and
    all the Relay stuff under "static/".
    
    Get full list:
    
        ag '/static/' public | \
          perl -pe 's#(/(?:static)/[-\w\d./]+)#\n\1\n#g' | \
          sort | \
          uniq | \
          less
    
    Then use our Bash trick to move the files:
    
        bash
    
        getit() {
          X=$1
          mkdir -p public/assets/$(dirname $X)
          curl https://wincent.com/$X -o public/assets/$X
        }
        export -f getit
    
        ag '/static/' public | \
          perl -pe 's#(/(?:static)/[-\w\d./]+)#\n\1\n#g' | \
          sort | \
          uniq | \
          perl -pe 's#^/##' | \
          xargs -n 1 -I {} sh -c 'getit {}'
    
    Then update all references to point to the new locations:
    
        perl -i -p -e 's#\bstatic/#assets/static/#g' public/**/*.css public/**/*.html
    
    And revert a couple of unwanted changes (`co` here is an alias for
    `checkout`):
    
        git co public/index.html
        git co public/issues/2161.html

---
+++ app/public/assets/static/normalize.css
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang='en-US'>
+<head>
+<meta content='text/html;charset=UTF-8' http-equiv='Content-Type'>
+<title>
+404 not found
+
+&middot; wincent.com
+</title>
+<link href='/assets/application-dfd4c4823a8b2979d87ab2ce01a9114c.css' media='screen' rel='stylesheet' type='text/css'>
+<script>
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-35574060-1']);
+  _gaq.push(['_trackPageview']);
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+</script>
+</head>
+<body>
+<nav class='global'>
+<h1>
+<a href='/'>Wincent</a>
+</h1>
+</nav>
+<div id='content-wrapper'>
+<div id='content'>
+<h1>Not found</h1>
+<p>
+The requested item could not be found.
+</p>
+<p>
+Please
+<a class='mailto' href='mailto:greg@hurrell.net'>contact me</a>
+if you think that you have found a broken link
+<em>within</em>
+wincent.com. If the broken link originates on another website please inform the originating site.
+</p>
+
+</div>
+</div>
+<footer class='global'>
+<ul>
+<li>
+<a class='mailto' href='mailto:greg@hurrell.net' title='Email me at greg@hurrell.net'>contact</a>
+</li>
+<li>
+<a href='/misc/legal'>legal</a>
+</li>
+</ul>
+</footer>
+</body>
+</html>
