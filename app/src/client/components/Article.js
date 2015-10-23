97b2c52293b8a28196a8be31457d790763365a1c
Greg Hurrell <greg@hurrell.net>
2015-10-22 20:44:11 -0700

    Handle other kinds of redirect

--- app/src/client/components/Article.js
+++ app/src/client/components/Article.js
@@ -9,9 +9,33 @@ import When from './When';
 import './Article.css';
 
 class Article extends React.Component {
+  static contextTypes = {
+    history: React.PropTypes.object,
+  };
+
   render() {
     const {article} = this.props;
-    // TODO handle redirects
+
+    if (article.redirect) {
+      if (article.redirect.match(/^https?:/)) {
+        // External redirect.
+        window.location = article.redirect;
+        return null;
+      } else if (article.redirect.substr(0, 1) === '/') {
+        // Internal redirect.
+        const {history} = this.context;
+        const location = history.createLocation(article.redirect);
+        history.match(location, (error, redirectLocation, nextState) => {
+          if (nextState) {
+            history.pushState({}, article.redirect);
+          } else {
+            window.location = article.redirect;
+          }
+        });
+        return null;
+      }
+    }
+
     return (
       <DocumentTitle isLeaf={true} title={article.title}>
         <article>
