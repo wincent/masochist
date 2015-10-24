12a7fe50ec515958613939d1e053a52ca681a9c3
Greg Hurrell <greg@hurrell.net>
2015-10-23 22:52:41 -0700

    Make tests runnable with `npm test`

---
+++ app/src/__mocks__/sinon.js
@@ -0,0 +1,27 @@
+'use strict';
+
+import sinon from 'sinon';
+
+let sandbox;
+
+beforeEach(() => {
+  sandbox = sinon.sandbox.create();
+});
+
+afterEach(() => {
+  sandbox.restore();
+});
+
+global.sinon = {
+  spy() {
+    return sinon.spy(...arguments);
+  },
+
+  stub() {
+    return sandbox.stub(...arguments);
+  },
+
+  useFakeTimers() {
+    return sinon.useFakeTimers(...arguments);
+  }
+};
