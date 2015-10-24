a87b4c03670bffc871c8abf909bfd5670ba24791
Greg Hurrell <greg@hurrell.net>
2015-10-23 23:06:02 -0700

    Drop unnecessary field from <Article> component

--- app/src/client/components/Article.js
+++ app/src/client/components/Article.js
@@ -67,7 +67,6 @@ export default Relay.createContainer(Article, {
   fragments: {
     article: () => Relay.QL`
       fragment on Article {
-        id
         title
         redirect
         resolvedTitle
