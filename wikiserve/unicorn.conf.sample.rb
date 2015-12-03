b8dc7de86ebf6aa109842a4c8acec959f50848a2
Greg Hurrell <greg@hurrell.net>
2015-12-02 23:17:04 -0800

    wikiserve: make sample unicorn conf more prod-like
    Switching out "/var" for "/data", and making sure the
    `working_directory` config reflects reality (where the wikiserve app is
    running out of a subdirectory in the repo).

--- wikiserve/unicorn.conf.sample.rb
+++ wikiserve/unicorn.conf.sample.rb
@@ -1,7 +1,6 @@
 env = ENV['RACK_ENV'] || 'production'
 worker_processes(env == 'production' ? 4 : 1)
-#listen '/data/wikiserve/deploy/shared/unicorn.sock'
-working_directory '/data/wikiserve/deploy/current'
-pid '/data/wikiserve/deploy/shared/pids/unicorn.pid'
-stderr_path '/var/log/unicorn/wikiserve.stderr.log'
-stdout_path '/var/log/unicorn/wikiserve.stdout.log'
+working_directory '/var/wikiserve/deploy/current/wikiserve'
+pid '/var/run/wikiserve/wikiserve.pid'
+stderr_path '/var/log/wikiserve/unicorn.stderr.log'
+stdout_path '/var/log/wikiserve/unicorn.stdout.log'
