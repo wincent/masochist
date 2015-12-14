15a1dfeddd801d75eb7cbf8b6ec0c1b8ea667817
Greg Hurrell <greg@hurrell.net>
2015-12-13 23:42:17 -0800

    Fix failing test
    Started failing when `getCanonicalURLForRequest` became
    async.

--- app/src/server/__tests__/getCanonicalURLForRequest-test.js
+++ app/src/server/__tests__/getCanonicalURLForRequest-test.js
@@ -2,8 +2,12 @@ import getCanonicalURLForRequest from '../getCanonicalURLForRequest';
 
 describe('getCanonicalURLForRequest()', () => {
   it('maps / to /blog', () => {
-    expect(getCanonicalURLForRequest({path: '/'})).toBe('https://wincent.com/blog');
+    return getCanonicalURLForRequest({path: '/'})
+      .then(url => expect(url).toBe('https://wincent.com/blog'));
   });
 
-  // TODO more tests here
+  it('strips trailing slashes', () => {
+    return getCanonicalURLForRequest({path: '/blog/'})
+      .then(url => expect(url).toBe('https://wincent.com/blog'));
+  });
 });
