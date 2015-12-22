a2335101f535d56a6817acca4cf2256572d50d34
Greg Hurrell <greg@hurrell.net>
2015-12-22 10:38:45 -0800

    Cache feed
    Key is a combo of content hash plus an internal version number that we
    can bump if we ever want to change the format or fix a bug in the
    generation.
    
    Note that I switched the format to always indent because I didn't want
    to risk having varying content in the cache depending on whether I'm in
    `__DEV__` or not.
    
    Fixes: https://github.com/wincent/masochist/issues/67

--- app/src/server/actions/feed.js
+++ app/src/server/actions/feed.js
@@ -4,10 +4,12 @@
 
 import entities from 'entities';
 import RSS from 'rss';
+import Cache from '../../common/Cache';
 import {
   canonicalHost,
   canonicalScheme,
 } from '../../common/config';
+import git from '../git';
 import runQuery from '../runQuery';
 import stripTags from '../stripTags';
 
@@ -30,41 +32,54 @@ function extractExcerpt(body: string): string {
   );
 }
 
+/**
+ * Separate cache breaker from the one in the Cache module allows us to change
+ * the format of the feed without invalidating the entire cache.
+ */
+const FEED_VERSION = 1;
+
 export default async function feed() {
-  const result = await runQuery(`
-    query FeedQuery {
-      viewer {
-        posts(first: 10) {
-          edges {
-            node {
-              body {
-                html
+  const head = (await git('rev-parse', 'content')).trim();
+  const key = `blog:rss:${FEED_VERSION}:${head}`;
+  return await Cache.get(
+    key,
+    async () => {
+      const result = await runQuery(`
+        query FeedQuery {
+          viewer {
+            posts(first: 10) {
+              edges {
+                node {
+                  body {
+                    html
+                  }
+                  createdAt
+                  title
+                  url
+                }
               }
-              createdAt
-              title
-              url
             }
           }
         }
-      }
+      `);
+
+      const feed = new RSS({
+        feed_url: canonicalScheme + canonicalHost + '/blog.rss',
+        generator: 'Masochist',
+        site_url: canonicalScheme + canonicalHost + '/blog',
+        title: 'wincent.com blog',
+      });
+
+      result.data.viewer.posts.edges.forEach(({node}) => {
+        feed.item({
+          date: node.createdAt,
+          description: extractExcerpt(node.body.html),
+          title: node.title,
+          url: canonicalScheme + canonicalHost + node.url,
+        });
+      });
+
+      return feed.xml({indent: true});
     }
-  `);
-
-  const feed = new RSS({
-    feed_url: canonicalScheme + canonicalHost + '/blog.rss',
-    generator: 'Masochist',
-    site_url: canonicalScheme + canonicalHost + '/blog',
-    title: 'wincent.com blog',
-  });
-
-  result.data.viewer.posts.edges.forEach(({node}) => {
-    feed.item({
-      date: node.createdAt,
-      description: extractExcerpt(node.body.html),
-      title: node.title,
-      url: canonicalScheme + canonicalHost + node.url,
-    });
-  });
-
-  return __DEV__ ? feed.xml({indent: true}) : feed.xml();
+  );
 }
