e2f1ca52b9b00f647e0bda17a86d50ebe3758285
Greg Hurrell <greg@hurrell.net>
2016-06-21 06:41:47 -0700

    Revert "Update bundle in preparation for isomorphic rendering"
    Ugh, too much shit be broken by the wholesale upgrade of All The Things at once.
    Going to try doing this in a more piecemeal way.
    
    This reverts commit afacfb23515d0cd2a6064409a358e637e2031140.

--- app/src/client/app.js
+++ app/src/client/app.js
@@ -8,11 +8,11 @@ import {useRouterHistory} from 'react-router';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {RelayRouter} from 'react-router-relay';
-// import useStandardScroll from 'scroll-behavior/lib/useStandardScroll';
+import useStandardScroll from 'scroll-behavior/lib/useStandardScroll';
 import createBrowserHistory from 'history/lib/createBrowserHistory';
 import routeConfig from '../common/routeConfig';
 
-const history = /*useStandardScroll(*/useRouterHistory(createBrowserHistory)/*)*/();
+const history = useStandardScroll(useRouterHistory(createBrowserHistory))();
 
 ReactDOM.render(
   <RelayRouter history={history} routes={routeConfig} />,
