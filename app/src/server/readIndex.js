359bcd9577cecf419acf872575c55760484bed2b
Greg Hurrell <greg@hurrell.net>
2017-05-11 01:38:27 -0700

    Fix a crazy Flow error
    Yay for types. Flow was quite happy about this, but my runtime checks
    were not, catching an error in the types.

--- app/src/server/readIndex.js
+++ app/src/server/readIndex.js
@@ -2,10 +2,10 @@
  * @flow
  */
 
-import {number, string} from '../common/checks';
+import {array, number} from '../common/checks';
 import redis from '../common/redis';
 
-export type IndexResult = [number, string];
+export type IndexResult = [Array<mixed>, number];
 
 export default (async function readIndex(
   name: string,
@@ -19,5 +19,5 @@ export default (async function readIndex(
 
   // Results is not an array, so we can't destructure it (although we can make
   // it into an array for the benefit of our callers).
-  return [number(results[0]), string(results[1])];
+  return [array(results[0]), number(results[1])];
 });
