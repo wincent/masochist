b5b689255f2e484a858bde20cc43b4ef8ffba8c8
Greg Hurrell <greg@hurrell.net>
2017-05-18 07:09:27 -0700

    Move createdAt/updatedAt fields into Versioned interface
    No changes to queries needed; just makes more sense for this to belong
    here. I plan to take advantaged of this in refactoring the `<When>`
    component.

--- app/src/server/schema/fields/timestampFields.js
+++ app/src/server/schema/fields/timestampFields.js
@@ -6,10 +6,12 @@ import DateTimeType from '../types/DateTimeType';
 
 const timestampFields = {
   createdAt: {
+    description: 'When the content was first created',
     type: DateTimeType,
     resolve: (record: Object): ?Date => record.createdAt,
   },
   updatedAt: {
+    description: 'When the content was last updated',
     type: DateTimeType,
     resolve: (record: Object): ?Date => record.updatedAt,
   },
--- app/src/server/schema/interfaces/versionedInterface.js
+++ app/src/server/schema/interfaces/versionedInterface.js
@@ -3,15 +3,24 @@
  */
 
 import {GraphQLInterfaceType, GraphQLObjectType, GraphQLString} from 'graphql';
+import DateTimeType from '../types/DateTimeType';
 import HistoryType from '../types/HistoryType';
 
 const versionedInterface = new GraphQLInterfaceType({
   name: 'Versioned',
-  description: 'An object with a history field',
+  description: 'An object with history and timestamp fields',
   fields: {
+    createdAt: {
+      type: DateTimeType,
+      description: 'When the content was first created',
+    },
     history: {
       type: HistoryType,
       description: 'Revision history for the object',
+    },
+    updatedAt: {
+      type: DateTimeType,
+      description: 'When the content was last updated',
     },
   },
 });
