24d0f0f66535c1a829447bae5d8a52e8d1f4abcf
Greg Hurrell <greg@hurrell.net>
2017-09-28 23:45:42 -0700

    Strip out most of the guts of the compiler

--- scripts/compile.js
+++ scripts/compile.js
@@ -26,7 +26,6 @@ const WatchmanClient = require('relay-compiler/lib/GraphQLWatchmanClient');
 const formatGeneratedModule = require('relay-compiler/lib/formatGeneratedModule');
 const fs = require('fs');
 const path = require('path');
-const yargs = require('yargs');
 
 const {
   buildASTSchema,
@@ -45,84 +44,43 @@ const {
 
 import type {GraphQLSchema} from 'graphql';
 
-function buildWatchExpression(options: {
-  extensions: Array<string>,
-  include: Array<string>,
-  exclude: Array<string>,
-}) {
+function persistQuery(text: string): Promise<string> {
+  const match = text.match(/^\s*query\s+(\w+Query)\b/);
+  if (!match) {
+    console.error('Failed to find query name in text:\n' + text);
+    throw new Error('Failed to find query name in text');
+  }
+  const name = match[1];
+  const queryDir = path.resolve(process.cwd(), 'src/__generated__');
+  if (!fs.existsSync(queryDir)) {
+    fs.mkdirSync(queryDir);
+  }
+  fs.writeFileSync(path.join(queryDir, name + '.txt'), text);
+  return Promise.resolve(name);
+}
+
+function buildWatchExpression() {
   return [
     'allof',
     ['type', 'f'],
-    ['anyof', ...options.extensions.map(ext => ['suffix', ext])],
-    [
-      'anyof',
-      ...options.include.map(include => ['match', include, 'wholename']),
-    ],
-    ...options.exclude.map(exclude => ['not', ['match', exclude, 'wholename']]),
+    ['anyof', ['suffix', 'js']],
+    ['not', ['match', '**/node_modules/**', 'wholename']],
+    ['not', ['match', '**/__mocks__/**', 'wholename']],
+    ['not', ['match', '**/__tests__/**', 'wholename']],
+    ['not', ['match', '**/__generated__/**', 'wholename']],
   ];
 }
 
-function getFilepathsFromGlob(
-  baseDir,
-  options: {
-    extensions: Array<string>,
-    include: Array<string>,
-    exclude: Array<string>,
-  },
-): Array<string> {
-  const {extensions, include, exclude} = options;
-  const patterns = include.map(inc => `${inc}/*.+(${extensions.join('|')})`);
-
-  // $FlowFixMe(site=react_native_fb,www)
-  const glob = require('fast-glob');
-  return glob.sync(patterns, {
-    cwd: baseDir,
-    bashNative: [],
-    onlyFiles: true,
-    ignore: exclude,
-  });
-}
-
-async function run(options: {
-  schema: string,
-  src: string,
-  extensions: Array<string>,
-  include: Array<string>,
-  exclude: Array<string>,
-  verbose: boolean,
-  watchman: boolean,
-  watch?: ?boolean,
-  validate: boolean,
-}) {
-  const schemaPath = path.resolve(process.cwd(), options.schema);
+async function run() {
+  const schemaPath = path.resolve(process.cwd(), 'schema.graphql');
   if (!fs.existsSync(schemaPath)) {
-    throw new Error(`--schema path does not exist: ${schemaPath}.`);
+    throw new Error(`schema.graphql does not exist: ${schemaPath}.`);
   }
-  const srcDir = path.resolve(process.cwd(), options.src);
+  const srcDir = path.resolve(process.cwd(), 'src');
   if (!fs.existsSync(srcDir)) {
-    throw new Error(`--source path does not exist: ${srcDir}.`);
+    throw new Error(`source path does not exist: ${srcDir}.`);
   }
-  if (options.watch && !options.watchman) {
-    throw new Error('Watchman is required to watch for changes.');
-  }
-  if (options.watch && !hasWatchmanRootFile(srcDir)) {
-    throw new Error(
-      `
---watch requires that the src directory have a valid watchman "root" file.
-
-Root files can include:
-- A .git/ Git folder
-- A .hg/ Mercurial folder
-- A .watchmanconfig file
-
-Ensure that one such file exists in ${srcDir} or its parents.
-    `.trim(),
-    );
-  }
-
-  const reporter = new ConsoleReporter({verbose: options.verbose});
-
-  const useWatchman = options.watchman && (await WatchmanClient.isAvailable());
+  const reporter = new ConsoleReporter({verbose: true});
 
   const parserConfigs = {
     default: {
@@ -130,8 +88,8 @@ Ensure that one such file exists in ${srcDir} or its parents.
       getFileFilter: RelayJSModuleParser.getFileFilter,
       getParser: RelayJSModuleParser.getParser,
       getSchema: () => getSchema(schemaPath),
-      watchmanExpression: useWatchman ? buildWatchExpression(options) : null,
-      filepaths: useWatchman ? null : getFilepathsFromGlob(srcDir, options),
+      watchmanExpression: buildWatchExpression(),
+      filepaths: null,
     },
   };
   const writerConfigs = {
@@ -146,21 +104,11 @@ Ensure that one such file exists in ${srcDir} or its parents.
     reporter,
     parserConfigs,
     writerConfigs,
-    onlyValidate: options.validate,
+    onlyValidate: false,
   });
-  if (!options.validate && !options.watch && options.watchman) {
-    // eslint-disable-next-line no-console
-    console.log('HINT: pass --watch to keep watching for changes.');
-  }
-  const result = options.watch
-    ? await codegenRunner.watchAll()
-    : await codegenRunner.compileAll();
-
+  const result = await codegenRunner.compileAll();
   if (result === 'ERROR') {
     process.exit(100);
-  }
-  if (options.validate && result !== 'NO_CHANGES') {
-    process.exit(101);
   }
 }
 
@@ -210,89 +158,7 @@ ${error.stack}
   }
 }
 
-// Ensure that a watchman "root" file exists in the given directory
-// or a parent so that it can be watched
-const WATCHMAN_ROOT_FILES = ['.git', '.hg', '.watchmanconfig'];
-function hasWatchmanRootFile(testPath) {
-  while (path.dirname(testPath) !== testPath) {
-    if (
-      WATCHMAN_ROOT_FILES.some(file => {
-        return fs.existsSync(path.join(testPath, file));
-      })
-    ) {
-      return true;
-    }
-    testPath = path.dirname(testPath);
-  }
-  return false;
-}
-
-// Collect args
-const argv = yargs
-  .usage(
-    'Create Relay generated files\n\n' +
-      '$0 --schema <path> --src <path> [--watch]',
-  )
-  .options({
-    schema: {
-      describe: 'Path to schema.graphql or schema.json',
-      demandOption: true,
-      type: 'string',
-    },
-    src: {
-      describe: 'Root directory of application code',
-      demandOption: true,
-      type: 'string',
-    },
-    include: {
-      array: true,
-      default: ['**'],
-      describe: 'Directories to include under src',
-      type: 'string',
-    },
-    exclude: {
-      array: true,
-      default: [
-        '**/node_modules/**',
-        '**/__mocks__/**',
-        '**/__tests__/**',
-        '**/__generated__/**',
-      ],
-      describe: 'Directories to ignore under src',
-      type: 'string',
-    },
-    extensions: {
-      array: true,
-      default: ['js'],
-      describe: 'File extensions to compile (--extensions js jsx)',
-      type: 'string',
-    },
-    verbose: {
-      describe: 'More verbose logging',
-      type: 'boolean',
-    },
-    watchman: {
-      describe: 'Use watchman when not in watch mode',
-      type: 'boolean',
-      default: true,
-    },
-    watch: {
-      describe: 'If specified, watches files and regenerates on changes',
-      type: 'boolean',
-    },
-    validate: {
-      describe:
-        'Looks for pending changes and exits with non-zero code instead of ' +
-        'writing to disk',
-      type: 'boolean',
-      default: false,
-    },
-  })
-  .help().argv;
-
-// Run script with args
-// $FlowFixMe: Invalid types for yargs. Please fix this when touching this code.
-run(argv).catch(error => {
+run().catch(error => {
   console.error(String(error.stack || error));
   process.exit(1);
 });
