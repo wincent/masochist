6896572b0377afe136e1d1852a160d0927122e79
Greg Hurrell <greg@hurrell.net>
2017-10-04 23:29:48 -0700

    Re-enable Flow in getRequestBody

--- src/common/getRequestBody.js
+++ src/common/getRequestBody.js
@@ -1,9 +1,9 @@
 /**
- * @noflow
+ * @flow
  */
 
 import stableStringify from './stableStringify';
 
-export default function getRequestBody({id}, variables) {
+export default function getRequestBody({id}: {id: string}, variables: {[string]: mixed}) {
   return stableStringify({id, variables});
 }
