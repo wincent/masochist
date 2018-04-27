323c858c9a61fdf91d850135b1498d230e2738c1
Greg Hurrell <greg@hurrell.net>
2018-04-26 21:41:10 -0700

    Make search results include hits in article titles
    Closes: https://github.com/wincent/masochist/issues/118

--- src/server/search.js
+++ src/server/search.js
@@ -6,8 +6,42 @@ import git, {GitError} from './git';
 
 type SearchResult = {
   id: string,
-  type: string,
+  type: 'blog' | 'pages' | 'snippets' | 'wiki',
 };
+
+let corpus = null;
+
+async function getCorpus(): Array<SearchResult> {
+  if (!corpus) {
+    const output = await git(
+      'ls-tree',
+      '-r', // Recurse.
+      '-z', // NUL-terminate.
+      '--name-only', // Ignore metadata.
+      'content', // Branch.
+      '--', // Subsequent arguments are paths.
+      'content/blog',
+      'content/pages',
+      'content/snippets',
+      'content/wiki',
+    );
+    corpus = [];
+    const regExp = new RegExp(
+      'content/' + // Prefix.
+      '([^/]+)/' + // Content type.
+      '([^\0]+)\\.\\w+\0', // Filename match + extension + terminator.
+      'g' // Match repeatedly.
+    );
+    let match;
+    while ((match = regExp.exec(output))) {
+      corpus.push({
+        id: match[2],
+        type: match[1],
+      });
+    }
+  }
+  return corpus;
+}
 
 /**
  * For ease of use, be liberal in what filters we accept.
@@ -53,6 +87,7 @@ export default (async function search(q: string): Promise<Array<SearchResult>> {
   }
 
   const directories = [];
+  const atoms = [];
 
   trimmed.split(/\s+/).forEach(atom => {
     const filter = getFilter(atom);
@@ -64,11 +99,34 @@ export default (async function search(q: string): Promise<Array<SearchResult>> {
       '-e', // Flag that next param is a search pattern.
       atom,
     );
+    atoms.push(new RegExp(atom, 'i')); // For title-based search below.
   });
   if (!directories.length) {
     directories.push('content');
   }
 
+  // Queries that only contain filters (eg. "type:blog") also return nothing.
+  if (!atoms.length) {
+    return [];
+  }
+
+  // Put title-based results in first, as they are likely more relevant.
+  const results = new Map();
+  let entries = await getCorpus();
+  atoms.every(atom => {
+    entries = entries.filter(entry => {
+      const {id, type} = entry;
+      return atom.test(id);
+    });
+    return entries.length;
+  });
+  entries.forEach(entry => {
+    const {id, type} = entry;
+    const key = `${type}/${id}`;
+    results.set(key, entry);
+  });
+
+  // Now add content-based results.
   const head = (await git('rev-parse', 'content')).trim();
   const tree = (await git('show', '-s', '--format=%T', head)).trim();
   let hits = '';
@@ -83,7 +141,6 @@ export default (async function search(q: string): Promise<Array<SearchResult>> {
   }
 
   // Extract hits into a more usable format.
-  const results = [];
   const regExp = new RegExp(
     // Tree SHA + separator.
     '[a-f0-9]{40}:' +
@@ -98,13 +155,15 @@ export default (async function search(q: string): Promise<Array<SearchResult>> {
   );
   let match;
   while ((match = regExp.exec(hits))) {
-    results.push({
-      id: match[2],
-      type: match[1],
-    });
+    const type = match[1];
+    const id = match[2];
+    const key = `${type}/${id}`;
+    if (!results.has(key)) {
+      results.set(key, {id, type});
+    }
   }
-  if (hits.length && (!results.length || regExp.lastIndex)) {
+  if (hits.length && (!results.size || regExp.lastIndex)) {
     throw new Error('Failed to consume input');
   }
-  return results;
+  return Array.from(results.values());
 });
