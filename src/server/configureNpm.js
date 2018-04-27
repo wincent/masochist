6462649a7eb8e1f14753b2bfe897c5f733dbb948
Greg Hurrell <greg@hurrell.net>
2018-04-26 21:09:07 -0700

    Fix broken Yarn detection
    Looks like Yarn don't set YARN_SILENT no more.
    
    As noted in the updated code comment, we only need this to deal with old
    global installs that people (like me) might be using (because they can't
    update their Yarn install thanks to the total war with Chef).

--- src/server/configureNpm.js
+++ src/server/configureNpm.js
@@ -3,19 +3,24 @@
  */
 
 /**
- * Yarn doesn't handle npm_package_config yet:
+ * Older versions of Yarn didn't handle npm_package_config:
  *
  *   https://github.com/yarnpkg/yarn/issues/2989
  *
- * As such, there are a few package.json scripts which *must* be run with `npm`
- * rather than `yarn`. This module detects running under `yarn` in those cases
+ * We have a new version installed under vendor/bin/, but it's possible people
+ * may invoke their globally installed, potentially older version of
+ * yarn instead.
+ *
+ * As such, for the few package.json scripts which *must* be run with `npm`
+ * rather than `yarn`, this module detects running under `yarn` in those cases
  * and shells out to `npm` to do the equivalent setting of environment
  * variables.
  */
 
 import {spawnSync} from 'child_process';
 
-if (process.env.hasOwnProperty('YARN_SILENT')) {
+const agent = process.env.npm_config_user_agent || '';
+if (agent.match(/\byarn\b/)) {
   const result = spawnSync('npm', ['config', 'get', 'masochist:content-repo']);
   if (result.status !== 0 || result.error) {
     throw new Error('configureNpm: Failed to get config');
