cb8c16374abc608fa70c898c87b22f2118d3e0d7
Greg Hurrell <greg@hurrell.net>
2018-11-23 23:44:21 +0100

    Suppress Flow complaint about React.memo until upstream issue is fixed
    One concern though: I am not getting unused suppression warnings like I
    would expect (if I introduce an unused one by way of testing). Not sure
    what is up with that.

--- src/client/components/TrustedPrerenderedMarkup.js
+++ src/client/components/TrustedPrerenderedMarkup.js
@@ -8,6 +8,7 @@ type Props = {
   html: string,
 };
 
+// $FlowIssue: https://github.com/facebook/flow/issues/7093
 export default React.memo(function TrustedPrerenderedMarkup({html}: Props) {
   return (
     <div className="prerendered" dangerouslySetInnerHTML={{__html: html}} />
