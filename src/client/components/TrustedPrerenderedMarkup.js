832090d25e0fc26f2f18ad87594f6d2266ec48fe
Greg Hurrell <greg@hurrell.net>
2018-11-23 12:31:16 +0100

    Move TrustedPrerenderedMarkup to React.memo

--- src/client/components/TrustedPrerenderedMarkup.js
+++ src/client/components/TrustedPrerenderedMarkup.js
@@ -8,19 +8,11 @@ type Props = {
   html: string,
 };
 
-// TODO: make this stateless functional (and probably others) -- will need
-// React.memo(fn)
-export default class TrustedPrerenderedMarkup extends React.Component<Props> {
-  shouldComponentUpdate(nextProps: Props) {
-    return this.props.html !== nextProps.html;
-  }
-
-  render() {
-    return (
-      <div
-        className="prerendered"
-        dangerouslySetInnerHTML={{__html: this.props.html}}
-      />
-    );
-  }
-}
+export default React.memo(function TrustedPrerenderedMarkup({html}: Props) {
+  return (
+    <div
+      className="prerendered"
+      dangerouslySetInnerHTML={{__html: html}}
+    />
+  );
+});
