883dab00bdf481d8acc29bda8081b29f89459848
Greg Hurrell <greg@hurrell.net>
2018-12-14 13:24:57 +0100

    Don't use "defaults" as browserslist target for @babel/preset-env
    Despite what the docs (https://babeljs.io/docs/en/babel-preset-env) say,
    it appears that not all the target queries supported by browserslist
    (https://github.com/browserslist/browserslist) can be used here.
    
    "default" is supposedly equivalent to "> 0.5%, last 2 versions, Firefox
    ESR, not dead", but when I add a `debug: true` flag, I see that it
    treats `targets` as `{}` and ends up producing a bundle which is a few
    KB larger (pre-gzip, I don't know about post-gzip). It looks to be the
    whole kaboodle. Disappointing that the list of included polyfills ends
    up being so large; a nice chunk gets trimmed off if you use a more
    aggressive target like "last 2 Chrome versions" (that's obviously not
    viable though). Over time, the bundle should get smaller though as more
    browsers drop off the list.
    
    Closes: https://github.com/wincent/masochist/issues/127

--- webpack.config.js
+++ webpack.config.js
@@ -23,8 +23,8 @@ module.exports = {
                 [
                   '@babel/preset-env',
                   {
+                    targets: '> 0.5%, last 2 versions, Firefox ESR, not dead',
                     useBuiltIns: 'entry',
-                    targets: 'defaults',
                   },
                 ],
                 '@babel/preset-react',
--- webpack.production.config.js
+++ webpack.production.config.js
@@ -80,8 +80,9 @@ module.exports = {
                 [
                   '@babel/preset-env',
                   {
+                    debug: false,
+                    targets: '> 0.5%, last 2 versions, Firefox ESR, not dead',
                     useBuiltIns: 'entry',
-                    targets: 'defaults',
                   },
                 ],
                 '@babel/preset-react',
