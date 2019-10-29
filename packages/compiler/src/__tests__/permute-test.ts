fbe87b65a522e7db9f9bcce908bf30d60a12e13b
Greg Hurrell <greg@hurrell.net>
2019-10-30 00:33:37 +0100

    style: reduce indentation in tests
    Just wanting to try this on for size, after about 15 years of slavishly
    using BDD-style "describe"/"it".

--- packages/compiler/src/__tests__/permute-test.ts
+++ packages/compiler/src/__tests__/permute-test.ts
@@ -1,26 +1,24 @@
 import permute from '../permute';
 
-describe('permute()', () => {
-  it('permutes an empty array', () => {
-    expect(permute([])).toEqual([[]]);
-  });
+test('permute an empty array', () => {
+  expect(permute([])).toEqual([[]]);
+});
 
-  it('permutes a array of one item', () => {
-    expect(permute(['foo'])).toEqual([['foo']]);
-  });
+test('permute a array of one item', () => {
+  expect(permute(['foo'])).toEqual([['foo']]);
+});
 
-  it('permutes a array of two items', () => {
-    expect(permute(['a', 'b'])).toEqual([['a', 'b'], ['b', 'a']]);
-  });
+test('permute a array of two items', () => {
+  expect(permute(['a', 'b'])).toEqual([['a', 'b'], ['b', 'a']]);
+});
 
-  it('permutes an array of 3 items', () => {
-    expect(permute([1, 2, 3])).toEqual([
-      [1, 2, 3],
-      [2, 1, 3],
-      [3, 1, 2],
-      [1, 3, 2],
-      [2, 3, 1],
-      [3, 2, 1],
-    ]);
-  });
+test('permute an array of 3 items', () => {
+  expect(permute([1, 2, 3])).toEqual([
+    [1, 2, 3],
+    [2, 1, 3],
+    [3, 1, 2],
+    [1, 3, 2],
+    [2, 3, 1],
+    [3, 2, 1],
+  ]);
 });
