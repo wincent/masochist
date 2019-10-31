5cbd0f22e0962be806ad29dac573c1c7287f9a58
Greg Hurrell <greg@hurrell.net>
2019-10-31 20:50:57 +0100

    test: add more tests

--- packages/compiler/src/__tests__/lex-test.ts
+++ packages/compiler/src/__tests__/lex-test.ts
@@ -22,3 +22,28 @@ test('lexing a comment', () => {
     {contents: '# this is a comment', index: 0, name: 'COMMENT'},
   ]);
 });
+
+test('lexing multiple comments', () => {
+  const tokens = [...lex(`
+    # this is a comment
+    # this is another
+  `)];
+
+  expect(tokens).toEqual([
+    {contents: '\n', index: 0, name: 'LINE_TERMINATOR'},
+    {contents: ' ', index: 1, name: 'WHITESPACE'},
+    {contents: ' ', index: 2, name: 'WHITESPACE'},
+    {contents: ' ', index: 3, name: 'WHITESPACE'},
+    {contents: ' ', index: 4, name: 'WHITESPACE'},
+    {contents: '# this is a comment', index: 5, name: 'COMMENT'},
+    {contents: '\n', index: 24, name: 'LINE_TERMINATOR'},
+    {contents: ' ', index: 25, name: 'WHITESPACE'},
+    {contents: ' ', index: 26, name: 'WHITESPACE'},
+    {contents: ' ', index: 27, name: 'WHITESPACE'},
+    {contents: ' ', index: 28, name: 'WHITESPACE'},
+    {contents: '# this is another', index: 29, name: 'COMMENT'},
+    {contents: '\n', index: 46, name: 'LINE_TERMINATOR'},
+    {contents: ' ', index: 47, name: 'WHITESPACE'},
+    {contents: ' ', index: 48, name: 'WHITESPACE'},
+  ]);
+});
