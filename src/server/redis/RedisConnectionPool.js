8c842b71a6f43955fd7355c0e9098d54467371e6
Greg Hurrell <greg@hurrell.net>
2021-12-12 23:25:55 +0100

    chore: remove dead code
    I initially thought I might use these, which correspond to the commands
    that I actually use, but in reality, I only use `get()`, and all the
    others get used via `multi()`.
    
    Now, when I start rewriting stuff in TypeScript, there will be some
    value in having named methods like this, although I will probably want
    to expose them on `multi()` like this:
    
        multi()
          .zrange(...)
          .zcard(...)
          .exec();
    
    as opposed to calling them individually (but having said that, I kind of
    like the way the array calling convention mirrors the array result, so I
    might see how far I can get with static types using that approach...)

--- src/server/redis/RedisConnectionPool.js
+++ src/server/redis/RedisConnectionPool.js
@@ -22,46 +22,12 @@ export default class RedisConnectionPool {
       },
 
       // Commands.
+      //
+      // Not a complete set, only the commands I actually use.
 
       // See: https://redis.io/commands/get
       get(key) {
         return this.command('GET', key);
-      },
-
-      // See: https://redis.io/commands/set
-      set(key, value) {
-        return this.command('SET', key, value);
-      },
-
-      // See: https://redis.io/commands/zadd
-      zadd(key, score, member) {
-        return this.command('ZADD', key, score, member);
-      },
-
-      // See: https://redis.io/commands/zcard
-      zcard(key) {
-        return this.command('ZCARD', key);
-      },
-
-      // See: https://redis.io/commands/zincrby
-      zincrby(key, increment, member) {
-        return this.command('ZINCRBY', key, increment, member);
-      },
-
-      // See: https://redis.io/commands/zrem
-      zrem(key, member) {
-        return this.command('ZREM', key, member);
-      },
-
-      // See: https://redis.io/commands/zremrangebyscore
-      zremrangebyscore(key, min, max) {
-        return this.command('ZREMRANGEBYSCORE', key, min, max);
-      },
-
-      // ZREVRANGE is deprecated, so we translate it to ZRANGE.
-      // See: https://redis.io/commands/zrevrange
-      zrevrange(key, start, stop) {
-        return this.command('ZRANGE', key, start, stop, 'REV');
       },
     };
   }
