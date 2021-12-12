6ed94a804269dc4f6f4915b3e02b1237fc25ab23
Greg Hurrell <greg@hurrell.net>
2021-12-12 23:19:49 +0100

    chore: remove commented-out test code

--- src/server/redis.js
+++ src/server/redis.js
@@ -22,37 +22,3 @@ export default {
     return client.multi(commandsWithPrefixedKeys);
   },
 };
-
-// TODO: remove these temporary tests...
-// Run them with: yarn run babel-node src/server/redis.js
-
-client.get('foo').then((result) => {
-  console.log('GET foo', result);
-});
-
-// This will open a second client because the last one hasn't been created yet.
-client
-  .get('masochist:6:last-indexed-hash')
-  .then((result) => {
-    console.log('GET masochist:6:last-indexed-hash', result);
-  })
-  .then(async () => {
-    // but this chained call will use an existing client
-    let result = await client.zcard('masochist:6:wiki-index');
-    console.log('ZCARD masochist:6:wiki-index', result);
-
-    // and this will too, because of the await
-    result = await client.get('bar');
-    console.log('GET bar', result);
-
-    result = await client.zrevrange('masochist:6:blog-index', 10, 20);
-    console.log('ZREVRANGE masochist:6:blog-index 10 20', result);
-
-    result = await client.multi([
-      ['ZREVRANGE', 'masochist:6:tag:pcre', 0, -1],
-      ['ZREVRANGE', 'masochist:6:tag:skype', 0, -1],
-      ['ZREVRANGE', 'masochist:6:tag:try', 0, -1],
-      ['ZREVRANGE', 'masochist:6:tag:znc', 0, -1],
-    ]);
-    console.log('multi (4 commands):', result);
-  });
