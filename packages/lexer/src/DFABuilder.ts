9a2733d478b71dc9342f8f234c073dffbcdf7adc
Greg Hurrell <greg@hurrell.net>
2021-12-27 21:07:48 +0100

    refactor: use state transition tables instead of adjacency list
    Because I think this is going to be easier to work with.

--- packages/lexer/src/DFABuilder.ts
+++ packages/lexer/src/DFABuilder.ts
@@ -10,86 +10,86 @@ import type {
   Sequence,
 } from './RegExp/RegExpParser';
 
-type State = Accept | Internal | Start;
+// State-transition table: a two dimensional array where first dimension is the
+// "from" state and the second dimension is the "to" state, with the value in
+// each cell indicating the transition conditions.
+//
+// See: https://en.wikipedia.org/wiki/State-transition_table
+type Conditions = Array<Atom | Range>;
+type Table = Array<Array<Conditions>>;
 
-type Accept = {kind: 'Accept'};
-type Internal = {kind: 'Internal'};
-type Start = {kind: 'Start'};
-
-type Transition = {
-  start: number;
-  end: number;
-  conditions: Array<Atom | Range>;
+// In other visitors we just call this "State" but I don't want anything that
+// could be confused with an NFA/DFA state, so being more explicit.
+type VisitorState = {
+  currentState: number;
+  table: Table;
 };
 
-type DFA = {
-  states: Array<State>;
-  transitions: Array<Transition>;
-};
-
-export default class DFABuilder extends RegExpVisitor<DFA> {
+export default class DFABuilder extends RegExpVisitor<VisitorState> {
   constructor(node: Node) {
     super(node);
   }
 
   build() {
-    const dfa = createDFA();
-    this.visit(dfa);
-    const last = dfa.states[dfa.states.length - 1];
-    if (last.kind !== 'Accept') {
-      (last.kind as any) = 'Accept';
+    // Start off with a single starting state containing an empty conditions
+    // array.
+    const visitorState: VisitorState = {
+      currentState: 0,
+      table: [[[]]],
+    };
+    this.visit(visitorState);
+    return visitorState.table;
+  }
+
+  visitAlternate(alternate: Alternate, visitorState: VisitorState) {
+    // TODO: this one is going to visit children
+    // we have to consolidate common transitions here
+    super.visitAlternate(alternate, visitorState);
+  }
+
+  visitAtom(atom: Atom, {currentState, table}: VisitorState) {
+    const nextState = this.#addState(table);
+
+    // Finally, record transition to new state.
+    const conditions = table[currentState][nextState];
+    conditions.push(atom);
+  }
+
+  visitCharacterClass(
+    characterClass: CharacterClass,
+    visitorState: VisitorState,
+  ) {
+    super.visitCharacterClass(characterClass, visitorState);
+  }
+
+  visitRepeat(repeat: Repeat, visitorState: VisitorState) {
+    super.visitRepeat(repeat, visitorState);
+  }
+
+  visitSequence(sequence: Sequence, visitorState: VisitorState) {
+    for (let i = 0; i < sequence.children.length; i++) {
+      const child = sequence.children[i];
+      this.visitNode(child, {
+        ...visitorState,
+        currentState: visitorState.currentState + i,
+      });
     }
-    return dfa;
   }
 
-  visitAlternate(alternate: Alternate, dfa: DFA) {
-    super.visitAlternate(alternate, dfa);
+  /**
+   * Creates a new state, growing both dimensions. Returns the index of the new
+   * state.
+   */
+  #addState(table: Table): number {
+    // Add new row.
+    const nextState = table.length;
+    table.push(new Array(nextState + 1).fill(null).map(() => []));
+
+    // Add new column to existing rows.
+    for (let i = 0; i < nextState; i++) {
+      table[i].push([]);
+    }
+
+    return nextState;
   }
-
-  visitAtom(atom: Atom, dfa: DFA) {
-    const start = dfa.states.length - 1;
-    const end = start + 1;
-    dfa.states.push({kind: 'Accept'});
-    dfa.transitions.push({start, end, conditions: [atom]});
-  }
-
-  visitCharacterClass(characterClass: CharacterClass, dfa: DFA) {
-    super.visitCharacterClass(characterClass, dfa);
-  }
-
-  visitRepeat(repeat: Repeat, dfa: DFA) {
-    super.visitRepeat(repeat, dfa);
-  }
-
-  visitSequence(sequence: Sequence, dfa: DFA) {
-    // Solve sub-problem.
-    const next = createDFA();
-    super.visitSequence(sequence, next);
-
-    // Merge back into current state.
-    const offset = dfa.states.length - 1;
-    dfa.states.push(
-      ...next.states.slice(1).map(({kind}) => {
-        return {
-          kind: kind === 'Accept' ? 'Internal' : kind,
-        };
-      }),
-    );
-    dfa.transitions.push(
-      ...next.transitions.map(({start, end, conditions}) => {
-        return {
-          start: start + offset,
-          end: end + offset,
-          conditions,
-        };
-      }),
-    );
-  }
-}
-
-function createDFA(): DFA {
-  return {
-    states: [{kind: 'Start'}],
-    transitions: [],
-  };
 }
--- packages/lexer/src/__tests__/DFABuilder-test.ts
+++ packages/lexer/src/__tests__/DFABuilder-test.ts
@@ -5,34 +5,22 @@ describe('DFABuilder()', () => {
   it('builds a DFA from a single atom', () => {
     const ast = compileRegExp(/a/);
     const builder = new DFABuilder(ast);
-    const dfa = builder.build();
-    expect(dfa).toEqual({
-      states: [{kind: 'Start'}, {kind: 'Accept'}],
-      transitions: [
-        {start: 0, end: 1, conditions: [{kind: 'Atom', value: 'a'}]},
-      ],
-    });
+    const table = builder.build();
+    expect(table).toEqual([
+      [[], [{kind: 'Atom', value: 'a'}]],
+      [[], []],
+    ]);
   });
 
   it('builds a DFA from a sequence', () => {
     const ast = compileRegExp(/foo/);
     const builder = new DFABuilder(ast);
-    const dfa = builder.build();
-    expect(dfa).toEqual({
-      states: [
-        // Note how this suggests that we don't even need to explicitly model
-        // states... by definition, state[0] can be "Start" and state[last] can
-        // be "Accept"; none of them require metadata to be associated.
-        {kind: 'Start'},
-        {kind: 'Internal'},
-        {kind: 'Internal'},
-        {kind: 'Accept'},
-      ],
-      transitions: [
-        {start: 0, end: 1, conditions: [{kind: 'Atom', value: 'f'}]},
-        {start: 1, end: 2, conditions: [{kind: 'Atom', value: 'o'}]},
-        {start: 2, end: 3, conditions: [{kind: 'Atom', value: 'o'}]},
-      ],
-    });
+    const table = builder.build();
+    expect(table).toEqual([
+      [[], [{kind: 'Atom', value: 'f'}], [], []],
+      [[], [], [{kind: 'Atom', value: 'o'}], []],
+      [[], [], [], [{kind: 'Atom', value: 'o'}]],
+      [[], [], [], []],
+    ]);
   });
 });
