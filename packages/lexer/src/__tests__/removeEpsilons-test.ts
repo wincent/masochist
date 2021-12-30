47bd565911e302f640b3e6e7f744d0ffc1c0e3be
Greg Hurrell <greg@hurrell.net>
2021-12-30 10:34:05 +0100

    test: add some tests

--- packages/lexer/src/__tests__/removeEpsilons-test.ts
+++ packages/lexer/src/__tests__/removeEpsilons-test.ts
@@ -2,6 +2,8 @@ import compileRegExp from '../compileRegExp';
 import regExpToNFA, {ACCEPT, NONE, START} from '../regExpToNFA';
 import removeEpsilons from '../removeEpsilons';
 
+import type {Flags, NFA} from '../regExpToNFA';
+
 describe('removeEpsilons()', () => {
   it('does nothing to an NFA created from an atom', () => {
     // No epsilons to remove.
@@ -67,6 +69,81 @@ describe('removeEpsilons()', () => {
                 },
               },
             ],
+          },
+        },
+      ],
+    });
+  });
+
+  it('removes epsilons from an NFA created from an alternate', () => {
+    expect(removeEpsilons(regExpToNFA(compileRegExp(/a|b|c/)))).toEqual({
+      id: 6,
+      flags: START,
+      edges: [
+        {
+          on: {kind: 'Atom', value: 'c'},
+          to: {
+            id: 5,
+            flags: ACCEPT,
+            edges: [],
+          },
+        },
+        {
+          on: {kind: 'Atom', value: 'b'},
+          to: {
+            id: 3,
+            flags: ACCEPT,
+            edges: [],
+          },
+        },
+        {
+          on: {kind: 'Atom', value: 'a'},
+          to: {
+            id: 1,
+            flags: ACCEPT,
+            edges: [],
+          },
+        },
+      ],
+    });
+  });
+
+  it('removes epsilons from an NFA created from a character class', () => {
+    expect(removeEpsilons(regExpToNFA(compileRegExp(/[a-z0]/)))).toEqual({
+      id: 4,
+      flags: START,
+      edges: [
+        {
+          on: {kind: 'Range', from: 'a', to: 'z'},
+          to: {
+            id: 3,
+            flags: ACCEPT,
+            edges: [],
+          },
+        },
+        {
+          on: {kind: 'Atom', value: '0'},
+          to: {
+            id: 1,
+            flags: ACCEPT,
+            edges: [],
+          },
+        },
+      ],
+    });
+  });
+
+  it('removes epsilons from an NFA created with a "?" quantifier', () => {
+    expect(removeEpsilons(regExpToNFA(compileRegExp(/a?/)))).toEqual({
+      id: 2,
+      flags: START | ACCEPT,
+      edges: [
+        {
+          on: {kind: 'Atom', value: 'a'},
+          to: {
+            id: 1,
+            flags: ACCEPT,
+            edges: [],
           },
         },
       ],
