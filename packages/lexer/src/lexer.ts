ac3e8cd1ef92958840d2ab120eb4318ce7f5f4d4
Greg Hurrell <greg@hurrell.net>
2022-01-07 19:23:49 +0100

    refactor: prep for next steps in lexer
    Just making some notes at this point.

--- packages/lexer/src/lexer.ts
+++ packages/lexer/src/lexer.ts
@@ -19,6 +19,33 @@ export const SOURCE_CHARACTER = /[\u0009\u000a\u000d\u0020-\uffff]/;
 export const UNICODE_BOM = /\ufeff/;
 export const WHITESPACE = /[\t ]+/;
 
+/**
+ * Generate a lexer for the GraphQL language.
+ *
+ * GraphQL is a simple language where almost all tokens can be recognized with a
+ * single character of lookahead. The only exceptions are:
+ *
+ * - BLOCK_STRING_VALUE and STRING_VALUE: These require three characters of
+ *   lookahead, to distinguish between the former (delimited by '"""') and the
+ *   latter (delimited by '"').
+ *
+ *   A STRING_VALUE may contain escapes, but a BLOCK_STRING_VALUE is effectively
+ *   "raw", other than some special whitespace handling dictated by the spec:
+ *   - https://spec.graphql.org/October2021/#sec-String-Value.Block-Strings
+ *
+ * - FLOAT_VALUE and INT_VALUE: An INT_VALUE may not be followed by a dot or a
+ *   NAME. As an added wrinkle, an INT_VALUE of zero may not be followed by
+ *   another digit (ie. "00" is invalid, as is "01" etc). If an INT_VALUE is
+ *   followed by a dot or "e"/"E" then it must be interpreted as a FLOAT_VALUE
+ *   instead.
+ *
+ *   A FLOAT_VALUE must not be followed by a dot (ie. "1.2.3" is invalid) or a
+ *   NAME (ie. "1.2L" is invalid).
+ *
+ *   See:
+ *   - https://spec.graphql.org/October2021/#sec-Int-Value
+ *   - https://spec.graphql.org/October2021/#sec-Float-Value
+ */
 export default generate(({ignored, token}) => {
   //
   // Punctuators (2.1.8)
@@ -46,8 +73,27 @@ export default generate(({ignored, token}) => {
   // TODO: figure out how to do this without back-tracking
   // about numbers: see recent changes to spec:
   // https://github.com/graphql/graphql-spec/pull/601
-  // FLOAT_VALUE: 1
-  // INT_VALUE: 1
+  //
+  // FLOAT_VALUE:
+  //
+  //    const FLOAT_VALUE = sequence(
+  //      'INTEGER_PART',
+  //      oneOf(
+  //        sequence('FRACTIONAL_PART', 'EXPONENT_PART'),
+  //        'EXPONENT_PART',
+  //        'FRACTIONAL_PART',
+  //      ),
+  //    );
+  //
+  // where the various `*_PART` patterns are defined as follows:
+  //
+  //    match(/[eE][+-]?\d+/).name('EXPONENT_PART');
+  //    match(/\.\d+/).name('FRACTIONAL_PART');
+  //    match(/-?(?:0|[1-9]\d*)/).name('INTEGER_PART');
+  //
+  // INT_VALUE:
+  //
+  //    const INT_VALUE = an('INTEGER_PART');
 
   token(
     'BLOCK_STRING_VALUE',
@@ -55,6 +101,20 @@ export default generate(({ignored, token}) => {
     // TODO: BLOCK_STRING_CHARACTER
     '"""',
   );
+  // cf previous lexer:
+  //
+  //    const BLOCK_STRING_VALUE = sequence(
+  //      match('"""'),
+  //      maybe(repeat('BLOCK_STRING_CHARACTER')),
+  //      match('"""'),
+  //    );
+  //
+  // where BLOCK_STRING_CHARACTER is:
+  //
+  //    oneOf(
+  //      match('\\"""'),
+  //      a('SOURCE_CHARACTER').except(oneOf(match('"""'), match('\\"""'))),
+  //    ).name('BLOCK_STRING_CHARACTER');
 
   token(
     'STRING_VALUE',
@@ -62,6 +122,23 @@ export default generate(({ignored, token}) => {
     // TODO: STRING_CHARACTER
     '"',
   );
+  // cf previous lexer:
+  //
+  //    const STRING_VALUE = sequence(
+  //      match('"'),
+  //      maybe(repeat('STRING_CHARACTER')),
+  //      match('"'),
+  //    );
+  //
+  // where STRING_CHARACTER is:
+  //
+  //    oneOf(
+  //      a('SOURCE_CHARACTER').except(
+  //        oneOf(match('"'), match('\\'), 'LINE_TERMINATOR'),
+  //      ),
+  //      'ESCAPED_UNICODE',
+  //      'ESCAPED_CHARACTER',
+  //    ).name('STRING_CHARACTER');
 
   //
   // Ignored tokens (2.1.7).
@@ -70,5 +147,5 @@ export default generate(({ignored, token}) => {
   ignored('WHITESPACE', WHITESPACE);
   ignored('LINE_TERMINATOR', LINE_TERMINATOR);
   ignored('COMMENT', COMMENT);
-  ignored('COMMA', ',');
+  ignored('COMMA', COMMA);
 });
