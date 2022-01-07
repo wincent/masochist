f797619bb452359b62d75553f457367984f49a74
Greg Hurrell <greg@hurrell.net>
2022-01-07 22:27:01 +0100

    fix: teach keyToTransition() to cope with ":" value in Atom
    Because we encode an atom "x" as "Atom:x", we run into strife when
    trying to encode atom ":" as "Atom::". Our `split(':')` call produces
    three components and that was causing an error to be thrown.

--- packages/lexer/src/NFA/keyToTransition.ts
+++ packages/lexer/src/NFA/keyToTransition.ts
@@ -12,33 +12,26 @@ export default function keyToTransition(transitionKey: string): Transition {
     return null;
   } else if (transitionKey === ANYTHING) {
     return {kind: 'Anything'};
-  } else {
-    const components = transitionKey.split(':');
-    if (components.length !== 2) {
+  } else if (transitionKey.startsWith(`${ATOM}:`)) {
+    const value = transitionKey.slice(`${ATOM}:`.length);
+    if (value.length !== 1) {
       throw new Error(
-        `keyToTransition(): Expected 2 components, got ${components.length}`,
+        `keyToTransition(): Expected Atom value length to be 1, was ${value.length}`,
       );
     }
-    const [kind, value] = components;
-    if (kind === ATOM) {
-      if (value.length !== 1) {
-        throw new Error(
-          `keyToTransition(): Expected Atom value length to be 1, was ${value.length}`,
-        );
-      }
-      return {kind: 'Atom', value};
-    } else if (kind === RANGE) {
-      if (value.length !== 3) {
-        throw new Error(
-          `keyToTransition(): Expected Range value length to be 3, was ${value.length}`,
-        );
-      }
-      if (value[1] !== '-') {
-        throw new Error('keyToTransition(): Unexpected Range value format');
-      }
-      return {kind: 'Range', from: value[0], to: value[2]};
-    } else {
-      throw new Error(`keyToTransition(): Unexpected kind: ${kind}`);
+    return {kind: 'Atom', value};
+  } else if (transitionKey.startsWith(`${RANGE}:`)) {
+    const value = transitionKey.slice(`${RANGE}:`.length);
+    if (value.length !== 3) {
+      throw new Error(
+        `keyToTransition(): Expected Range value length to be 3, was ${value.length}`,
+      );
     }
+    if (value[1] !== '-') {
+      throw new Error('keyToTransition(): Unexpected Range value format');
+    }
+    return {kind: 'Range', from: value[0], to: value[2]};
+  } else {
+    throw new Error(`keyToTransition(): Unexpected kind`);
   }
 }
