6246a38dc1b12a562c771385d7eb3295217b4a70
Greg Hurrell <greg@hurrell.net>
2022-02-02 01:26:02 +0100

    feat: implement inglorious hack to make `yarn lint` work
    What we want:
    
        yarn build # does a normal `tsc --build`
        yarn lint  # does a build with additional warnings turned on
    
    But this is surprisingly hard to do. What I had before didn't work:
    
        tsc --project tsconfig-lint.json
    
    because the settings defined in that file wouldn't actually override the
    ones in tsconfig-base.json. This may be because as a composite project,
    the subprojects would do `"extends": "../../tsconfig-base.json"` and
    never "see" the adjusted settings. (`yarn run tsc --showConfig` would
    show the warnings as on, but they didn't have any effect in the actual
    build).
    
    `tsc` is additionally very fussy about which options you can pass. For
    example, you can't pass `--build` and `--project` together. And you
    can't pass options like `--noUnusedLocals` with `--build`. Even if you
    could, not all settings have dual-mode command-line options; for
    example, you can pass `--allowUnreachableCode` but there is no way to
    express the opposite; ie. none of these work:
    
        --allowUnreachableCode no
        --allowUnreachableCode=no
        --allowUnreachableCode=false
        --noAllowUnreachableCode
        --no-allowUnreachableCode
    
    So, that leaves me with no other option. I am changing the "extends"
    hierarchy thus:
    
        tsconfig.json -> tsconfig-base.json -> tsconfig-dynamic.json
    
    When I want to run the linter, I overwrite tsconfig-dynamic.json with
    the desired settings, and restore them afterwards:
    
    - during linting: tsconfig-dynamic.json is overwritten with tsconfig-lint.json
    - after linting: tsconfig-dynamic.json is overwritten with tsconfig-empty.json
    
    Hideous, but it actually works, unlike everything else I tried.

---
+++ support/lint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+DIRECTORY=$(cd $(dirname "$BASH_SOURCE"); pwd)
+
+cd "$DIRECTORY/.."
+
+cp tsconfig-lint.json tsconfig-dynamic.json
+
+yarn tsc --build --force
+STATUS=$?
+
+cp tsconfig-empty.json tsconfig-dynamic.json
+
+exit $?
