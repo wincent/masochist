d9785a10894d14ca17f7f14a06ffb78f1ace14a6
Greg Hurrell <greg@hurrell.net>
2022-02-20 23:50:06 +0100

    docs(snippets): freshen arch-linux-install.sh snapshot

--- content/snippets/arch-linux-install.sh
+++ content/snippets/arch-linux-install.sh
@@ -45,6 +45,9 @@ ping -c 3 google.com
 log "Setting up NTP"
 timedatectl set-ntp true
 
+log "Updating mirror list"
+reflector -c Spain,France,Germany -a 6 --sort rate --save /etc/pacman.d/mirrorlist
+
 log "Refreshing packages"
 pacman -Syy
 
@@ -54,21 +57,20 @@ label: gpt
 device: /dev/nvme0n1
 
 /dev/nvme0n1p1: size=500MiB, type=uefi
-/dev/nvme0n1p2: size=50GiB, type=linux
-/dev/nvme0n1p3: type=linux
+/dev/nvme0n1p2: type=linux
 HERE
 
-log "Formatting partitions"
-mkfs.fat -F32 /dev/nvme0n1p1 # /boot/EFI
-mkfs.ext4 /dev/nvme0n1p2 # /
-mkfs.ext4 -O encrypt -b 4096 /dev/nvme0n1p3 # /home
+log "Formatting /boot"
+mkfs.fat -F32 /dev/nvme0n1p1
 
 log "Mounting /dev/nvme0n1p2 at /"
-mount /dev/nvme0n1p2 /mnt
+echo -n "${__PASSPHRASE__}" | cryptsetup luksFormat /dev/nvme0n1p2 -
+echo -n "${__PASSPHRASE__}" | cryptsetup open /dev/nvme0n1p2 cryptroot --key-file -
+mkfs.ext4 /dev/mapper/cryptroot
+mount /dev/mapper/cryptroot /mnt
 
-log "Mounting /dev/nvme0n1p3 at /home"
-mkdir /mnt/home
-mount /dev/nvme0n1p3 /mnt/home
+mkdir -p /mnt/boot
+mount /dev/nvme0n1p1 /mnt/boot
 
 log "Creating /etc/fstab"
 mkdir /mnt/etc
@@ -76,6 +78,8 @@ genfstab -U -p /mnt >> /mnt/etc/fstab
 
 log "Installing base packages"
 pacstrap /mnt base base-devel
+
+cp /etc/pacman.d/mirrorlist /mnt/etc/pacman.d/
 
 cat << HERE > /mnt/arch-install-chroot.sh
 set -e
@@ -90,13 +94,17 @@ log "Setting up database for 'pacman -F filename' searching"
 pacman -Fy
 
 log "Installing kernel and other packages"
-pacman -S --noconfirm linux linux-lts linux-headers linux-lts-headers
+pacman -S --noconfirm linux linux-lts linux-headers linux-lts-headers linux-firmware amd-ucode
 
 log "Installing other packages you want"
-pacman -S --noconfirm man-db
+pacman -S --noconfirm man-db terminus-font # for 4K display, instead of `setfont -d`
 
 log "Preparing ramdisks for kernel boot"
-# Note: this might be redundant; pacman already did it?
+sed -i 's/^HOOKS=(base udev autodetect modconf block filesystems keyboard fsck)/HOOKS=(base udev autodetect keyboard keymap modconf block encrypt filesystems fsck)/' /etc/mkinitcpio.conf
+
+echo FONT=ter-132n >> /etc/vconsole.conf
+echo KEYMAP=colemak >> /etc/vconsole.conf
+
 mkinitcpio -p linux
 mkinitcpio -p linux-lts
 
@@ -108,17 +116,22 @@ locale-gen
 
 log "Setting up users"
 echo "root:\$__PASSPHRASE__" | chpasswd
-useradd -m -g users -G wheel glh
-echo "glh:\$__PASSPHRASE__" | chpasswd
+useradd -m -g users -G wheel wincent
+echo "wincent:\$__PASSPHRASE__" | chpasswd
 echo '%wheel ALL=(ALL) ALL' > /etc/sudoers.d/wheel
 
 log "Setting up boot"
-pacman -S --noconfirm grub efibootmgr dosfstools os-prober mtools
-mkdir -p /boot/EFI
-mount /dev/nvme0n1p1 /boot/EFI
-grub-install --target=x86_64-efi --bootloader-id=grub_uefi --recheck
-cp /usr/share/locale/en\@quot/LC_MESSAGES/grub.mo /boot/grub/locale/en.mo
-grub-mkconfig -o /boot/grub/grub.cfg
+pacman -S --noconfirm efibootmgr dosfstools os-prober mtools
+
+PARTUUID=\$(lsblk /dev/nvme0n1p2 -o PARTUUID -d -n)
+
+efibootmgr --disk /dev/nvme0n1 --part 1 --create --label "Arch Linux LTS" --loader /vmlinuz-linux-lts --unicode "cryptdevice=PARTUUID=\${PARTUUID}:root root=/dev/mapper/root rw initrd=\initramfs-linux-lts.img" --verbose
+efibootmgr --disk /dev/nvme0n1 --part 1 --create --label "Arch Linux" --loader /vmlinuz-linux --unicode "cryptdevice=PARTUUID=\${PARTUUID}:root root=/dev/mapper/root rw initrd=\initramfs-linux.img" --verbose
+
+echo 'efibootmgr --disk /dev/nvme0n1 --part 1 --create --label "Arch Linux LTS" --loader /vmlinuz-linux-lts --unicode "cryptdevice=PARTUUID='\${PARTUUID}':root root=/dev/mapper/root rw initrd=\initramfs-linux-lts.img" --verbose' > /boot/b.sh
+echo 'efibootmgr --disk /dev/nvme0n1 --part 1 --create --label "Arch Linux" --loader /vmlinuz-linux --unicode "cryptdevice=PARTUUID='\${PARTUUID}':root root=/dev/mapper/root rw initrd=\initramfs-linux.img" --verbose' > /boot/a.sh
+echo \\\\vmlinuz-linux-lts cryptdevice=PARTUUID=\${PARTUUID}:root root=/dev/mapper/root rw initrd=\\\\initramfs-linux-lts.img > /boot/b.nsh
+echo \\\\vmlinuz-linux cryptdevice=PARTUUID=\${PARTUUID}:root root=/dev/mapper/root rw initrd=\\\\initramfs-linux.img > /boot/a.nsh
 
 log "Setting up swap"
 fallocate -l 2G /swapfile
@@ -126,16 +139,8 @@ chmod 600 /swapfile
 mkswap /swapfile
 echo -e '\n/swapfile none swap sw 0 0' >> /etc/fstab
 
-log "Setting up encryption for /home"
-pacman -S --noconfirm fscrypt
-fscrypt setup
-fscrypt setup /home
-echo "auth optional pam_fscrypt.so" >> /etc/pam.d/system-login
-echo "session optional pam_fscrypt.so drop_caches lock_policies" >> /etc/pam.d/system-login
-echo "password optional pam_fscrypt.so" >> /etc/pam.d/passwd
-
 log "Installing other dependencies"
-pacman -S --noconfirm git neovim ruby tmux vi vim xorg-server
+pacman -S --noconfirm git ruby tmux vi vim xorg-server
 
 pacman -S --noconfirm inetutils # for hostname
 pacman -S --noconfirm apcupsd # for auto-shutdown when UPS battery runs low
@@ -163,40 +168,12 @@ echo "IP=dhcp" >> "\$NETCTL_CONFIG"
 echo "Key=\\\\\"\$NETCTL_KEY" >> "\$NETCTL_CONFIG"
 netctl enable "\$NETCTL_PROFILE"
 
-log "Applying other settings"
-pacman -S --noconfirm terminus-font # for 4K display, instead of `setfont -d`
-echo FONT=ter-132n >> /etc/vconsole.conf
-echo KEYMAP=colemak >> /etc/vconsole.conf
-
 ln -sf /usr/share/zoneinfo/Europe/Madrid /etc/localtime
 hwclock --systohc
 
 log "Cloning dotfiles"
-sudo -u glh mkdir -p /home/glh/code
-sudo -u glh git clone --recursive https://github.com/wincent/wincent.git /home/glh/code/wincent
-
-log "Setting up /etc/motd"
-echo "Suggested actions:" >> /etc/motd
-echo "  sudo -s" >> /etc/motd
-echo "  mkdir /home/glh_" >> /etc/motd
-echo "  chown glh:users /home/glh_" >> /etc/motd
-echo "  fscrypt encrypt /home/glh_ --user=glh" >> /etc/motd
-echo "  exit" >> /etc/motd
-echo "  cp -a -T /home/glh /home/glh_" >> /etc/motd
-echo "  sudo reboot" >> /etc/motd
-echo "" >> /etc/motd
-echo "After rebooting:" >> /etc/motd
-echo "" >> /etc/motd
-echo "  fscrypt status /home/glh_" >> /etc/motd
-echo "  sudo mv /home/glh /home/glh_plaintext" >> /etc/motd
-echo "  sudo mv /home/glh_ /home/glh" >> /etc/motd
-echo "  sudo reboot" >> /etc/motd
-echo "" >> /etc/motd
-echo "And finally:" >> /etc/motd
-echo "" >> /etc/motd
-echo "  find /home/glh_plaintext -type f -print0 | xargs -0 shred -n1 --remove=unlink" >> /etc/motd
-echo "  sudo rm -rf /home/glh_plaintext" >> /etc/motd
-echo "  echo -n | sudo tee /etc/motd" >> /etc/motd
+sudo -u wincent mkdir -p /home/wincent/code
+sudo -u wincent git clone --recursive https://github.com/wincent/wincent.git /home/wincent/code/wincent
 
 exit
 HERE
