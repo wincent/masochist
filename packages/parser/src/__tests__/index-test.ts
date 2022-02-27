013796d807a752e76f03bcd14d7d4d33c5a63c29
Greg Hurrell <greg@hurrell.net>
2022-02-27 13:40:19 +0100

    refactor(parser): extract `getFollowSets()` into separate module

--- packages/parser/src/__tests__/index-test.ts
+++ packages/parser/src/__tests__/index-test.ts
@@ -3,7 +3,6 @@ import {dedent} from '@masochist/common';
 import {
   extendedGrammarForItemSets,
   getAugmentedGrammar,
-  getFollowSets,
   getItemSets,
   getParseTable,
   itemSetsToTransitionTable,
@@ -17,70 +16,6 @@ import {subsetGrammar, toyGrammar} from './grammars';
 import type {Token} from '..';
 
 // TODO: also add tests for full GraphQL grammar.
-
-describe('getFollowSets()', () => {
-  it('produces follow sets for the toy grammar', () => {
-    expect(getFollowSets(toyGrammar)).toEqual({
-      E: new Set(['eq', null]),
-      N: new Set([null]),
-      S: new Set([null]),
-      V: new Set(['eq', null]),
-    });
-  });
-
-  it('produces follow sets for the extended toy grammar', () => {
-    const itemSets = getItemSets(toyGrammar);
-    const extendedGrammar = extendedGrammarForItemSets(itemSets, toyGrammar);
-    expect(getFollowSets(extendedGrammar)).toEqual({
-      "0/S'/$": new Set([null]),
-      '0/V/3': new Set(['3/eq/7', null]),
-      '0/S/1': new Set([null]),
-      '0/N/2': new Set([null]),
-      '7/E/10': new Set([null]),
-      '5/E/8': new Set(['3/eq/7', null]),
-      '0/E/6': new Set([null]),
-      '5/V/9': new Set(['3/eq/7', null]),
-      '7/V/9': new Set([null]),
-    });
-  });
-
-  it('produces follow sets for the subset grammar', () => {
-    expect(getFollowSets(subsetGrammar)).toEqual({
-      Document: new Set([null]),
-      DefinitionList: new Set(['OPENING_BRACE', null]),
-      SelectionList: new Set(['CLOSING_BRACE', 'NAME']),
-      Definition: new Set(['OPENING_BRACE', null]),
-      ExecutableDefinition: new Set(['OPENING_BRACE', null]),
-      OperationDefinition: new Set(['OPENING_BRACE', null]),
-      SelectionSet: new Set(['OPENING_BRACE', null]),
-      Selection: new Set(['CLOSING_BRACE', 'NAME']),
-      Field: new Set(['CLOSING_BRACE', 'NAME']),
-    });
-  });
-
-  it('produces follow sets for the extended subset grammar', () => {
-    const itemSets = getItemSets(subsetGrammar);
-    const extendedGrammar = extendedGrammarForItemSets(itemSets, subsetGrammar);
-    expect(getFollowSets(extendedGrammar)).toEqual({
-      "0/Document'/$": new Set([null]),
-      '0/Document/1': new Set([null]),
-      '0/DefinitionList/2': new Set(['2/OPENING_BRACE/7', null]),
-      '7/SelectionList/9': new Set(['9/CLOSING_BRACE/13', '9/NAME/12']),
-      '0/Definition/3': new Set(['2/OPENING_BRACE/7', null]),
-      '2/Definition/8': new Set(['2/OPENING_BRACE/7', null]),
-      '0/ExecutableDefinition/4': new Set(['2/OPENING_BRACE/7', null]),
-      '0/OperationDefinition/5': new Set(['2/OPENING_BRACE/7', null]),
-      '0/SelectionSet/6': new Set(['2/OPENING_BRACE/7', null]),
-      '2/ExecutableDefinition/4': new Set(['2/OPENING_BRACE/7', null]),
-      '2/OperationDefinition/5': new Set(['2/OPENING_BRACE/7', null]),
-      '2/SelectionSet/6': new Set(['2/OPENING_BRACE/7', null]),
-      '7/Selection/10': new Set(['9/CLOSING_BRACE/13', '9/NAME/12']),
-      '9/Selection/14': new Set(['9/CLOSING_BRACE/13', '9/NAME/12']),
-      '7/Field/11': new Set(['9/CLOSING_BRACE/13', '9/NAME/12']),
-      '9/Field/11': new Set(['9/CLOSING_BRACE/13', '9/NAME/12']),
-    });
-  });
-});
 
 describe('getItemSets()', () => {
   it('produces items sets for the toy grammar', () => {
--- packages/parser/src/index.ts
+++ packages/parser/src/index.ts
@@ -2,16 +2,12 @@ import {StringScanner, invariant} from '@masochist/common';
 import vm from 'vm';
 
 import {RIGHTWARDS_ARROW} from './Grammar';
-import getFirstSets from './getFirstSets';
+import getFollowSets from './getFollowSets';
 import groupRulesByLHS from './groupRulesByLHS';
 
 // Goal is to produce an LALR(1) parser from a grammar.
 
 import type {Grammar} from './Grammar';
-
-type FollowSets = {
-  [nonTerminal: string]: Set<string | null>;
-};
 
 // TODO: Apart from the augmented rule, could avoid storing `lhs`/`rhs` and instead just store index of rule in original grammar.
 type Item = {
@@ -77,59 +73,6 @@ export const grammarDeclaration = `
     Field → NAME
 `;
 */
-
-export function getFollowSets(grammar: Grammar) {
-  const tokens = grammar.tokens;
-  const startRule = grammar.rules[0];
-  const first = getFirstSets(grammar);
-  const followSets: FollowSets = {
-    [startRule.lhs]: new Set([null]),
-  };
-
-  // For rule, "A → a B b", add everything in FIRST(b) to FOLLOW(B).
-  for (let i = 1; i < grammar.rules.length; i++) {
-    const {rhs} = grammar.rules[i];
-    for (let j = 0; j < rhs.length; j++) {
-      if (!tokens.has(rhs[j])) {
-        // Non-terminal.
-        if (rhs[j + 1]) {
-          for (const symbol of first[rhs[j + 1]] || [rhs[j + 1]]) {
-            followSets[rhs[j]] = followSets[rhs[j]] || new Set();
-            followSets[rhs[j]].add(symbol);
-          }
-        }
-      }
-    }
-  }
-
-  // For rule, "A -> a B", add everything in FOLLOW(A) to FOLLOW(B).
-  // Will keep looping until we stop making forward progress.
-  let done = false;
-  while (!done) {
-    done = true;
-    for (let i = 0; i < grammar.rules.length; i++) {
-      const {lhs, rhs} = grammar.rules[i];
-      for (let j = 0; j < rhs.length; j++) {
-        if (!tokens.has(rhs[j])) {
-          // Non-terminal.
-          if (!rhs[j + 1]) {
-            if (followSets[lhs]) {
-              for (const symbol of followSets[lhs]) {
-                followSets[rhs[j]] = followSets[rhs[j]] || new Set();
-                if (!followSets[rhs[j]].has(symbol)) {
-                  followSets[rhs[j]].add(symbol);
-                  done = false;
-                }
-              }
-            }
-          }
-        }
-      }
-    }
-  }
-
-  return followSets;
-}
 
 /**
  * Augment the grammar by starting with a new rule that leads to the original
