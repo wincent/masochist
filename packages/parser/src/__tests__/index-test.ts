b01357036cb6611790caf0ab0b1491e68277632b
Greg Hurrell <greg@hurrell.net>
2022-02-27 14:12:14 +0100

    test(parser): extract `itemSetsToTransitionTable()` tests
    I was supposed to do this in 276712c90, but I overlooked it, and the
    tests were passing because I'd adjusted all the relevant paths.

--- packages/parser/src/__tests__/index-test.ts
+++ packages/parser/src/__tests__/index-test.ts
@@ -4,83 +4,10 @@ import {stringifyGrammar, stringifyParseTable} from '..';
 import getAugmentedGrammar from '../getAugmentedGrammar';
 import getItemSets from '../getItemSets';
 import getParseTable from '../getParseTable';
-import {subsetGrammar, toyGrammar} from './grammars';
+import {toyGrammar} from './grammars';
 import itemSetsToTransitionTable from '../itemSetsToTransitionTable';
 
 // TODO: also add tests for full GraphQL grammar.
-
-describe('itemSetsToTransitionTable()', () => {
-  it('returns a transition table for the toy grammar', () => {
-    const itemSets = getItemSets(toyGrammar);
-    expect(itemSetsToTransitionTable(itemSets, toyGrammar)).toEqual([
-      /* 0 */ {
-        E: 6,
-        N: 2,
-        S: 1,
-        V: 3,
-        star: 5,
-        x: 4,
-      },
-      /* 1 */ {},
-      /* 2 */ {},
-      /* 3 */ {
-        eq: 7,
-      },
-      /* 4 */ {},
-      /* 5 */ {
-        E: 8,
-        V: 9,
-        star: 5,
-        x: 4,
-      },
-      /* 6 */ {},
-      /* 7 */ {
-        E: 10,
-        V: 9,
-        star: 5,
-        x: 4,
-      },
-      /* 8 */ {},
-      /* 9 */ {},
-      /* 10 */ {},
-    ]);
-  });
-
-  it('returns a transition table for the subset grammar', () => {
-    const itemSets = getItemSets(subsetGrammar);
-    expect(itemSetsToTransitionTable(itemSets, subsetGrammar)).toEqual([
-      /* 0 */ {
-        Definition: 3,
-        DefinitionList: 2,
-        Document: 1,
-        ExecutableDefinition: 4,
-        OPENING_BRACE: 7,
-        OperationDefinition: 5,
-        SelectionSet: 6,
-      },
-      /* 1 */ {},
-      /* 2 */ {
-        Definition: 8,
-        ExecutableDefinition: 4,
-        OPENING_BRACE: 7,
-        OperationDefinition: 5,
-        SelectionSet: 6,
-      },
-      /* 3 */ {},
-      /* 4 */ {},
-      /* 5 */ {},
-      /* 6 */ {},
-      /* 7 */ {Field: 11, NAME: 12, Selection: 10, SelectionList: 9},
-      /* 8 */ {},
-      /* 9 */ {CLOSING_BRACE: 13, Field: 11, NAME: 12, Selection: 14},
-      /* 10 */ {},
-      /* 11 */ {},
-      /* 12 */ {},
-      /* 13 */ {},
-      /* 14 */ {},
-    ]);
-  });
-});
 
 describe('stringifyGrammar()', () => {
   it('returns a stringified toy grammar', () => {
