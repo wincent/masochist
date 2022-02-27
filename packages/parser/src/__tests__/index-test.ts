2a90a4437e19a5cdd06e25488d006805ea362c4c
Greg Hurrell <greg@hurrell.net>
2022-02-27 18:23:44 +0100

    refactor(parser): prepare to extend grammar
    For now this is still basically the same as the subset grammar, but my
    plan is to slowly augment it until it covers the whole spec.

--- packages/parser/src/__tests__/index-test.ts
+++ packages/parser/src/__tests__/index-test.ts
@@ -1,3 +1,79 @@
+import {table, grammar} from '..';
+import parseWithTable from '../parseWithTable';
+
+import type {Token} from '../parseWithTable';
+
 describe('GraphQL parser', () => {
-  xtest('parser parses full GraphQL grammar', () => {});
+  it('parses a simple anonymous query', () => {
+    const tokens: Array<Token> = [
+      {name: 'OPENING_BRACE'},
+      {name: 'NAME', contents: 'foo'},
+      {name: 'NAME', contents: 'bar'},
+      {name: 'NAME', contents: 'baz'},
+      {name: 'CLOSING_BRACE'},
+    ];
+
+    expect(parseWithTable(table, tokens, grammar)).toEqual({
+      kind: 'Document',
+      children: [
+        {
+          kind: 'Definition',
+          children: [
+            {
+              kind: 'ExecutableDefinition',
+              children: [
+                {
+                  kind: 'OperationDefinition',
+                  children: [
+                    {
+                      kind: 'Selection',
+                      children: [
+                        {
+                          kind: 'Field',
+                          children: [
+                            {
+                              name: 'NAME',
+                              contents: 'foo',
+                            },
+                          ],
+                        },
+                      ],
+                    },
+                    {
+                      kind: 'Selection',
+                      children: [
+                        {
+                          kind: 'Field',
+                          children: [
+                            {
+                              name: 'NAME',
+                              contents: 'bar',
+                            },
+                          ],
+                        },
+                      ],
+                    },
+                    {
+                      kind: 'Selection',
+                      children: [
+                        {
+                          kind: 'Field',
+                          children: [
+                            {
+                              name: 'NAME',
+                              contents: 'baz',
+                            },
+                          ],
+                        },
+                      ],
+                    },
+                  ],
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    });
+  });
 });
--- packages/parser/src/index.ts
+++ packages/parser/src/index.ts
@@ -1,52 +1,41 @@
-// Goal is to produce an LALR(1) parser from a grammar.
+import getItemSets from './getItemSets';
+import getParseTable from './getParseTable';
+import itemSetsToTransitionTable from './itemSetsToTransitionTable';
+import parseDSL from './parseDSL';
 
 /**
  * Based on: https://spec.graphql.org/October2021/#sec-Document-Syntax
  */
-/*
-export const grammar: Grammar = {
-  tokens: new Set(['CLOSING_BRACE', 'NAME', 'OPENING_BRACE']),
-  rules: [
-    {lhs: 'Document', rhs: ['DefinitionList']},
-    {lhs: 'DefinitionList', rhs: ['Definition']},
-    {
-      lhs: 'DefinitionList',
-      rhs: ['DefinitionList', 'Definition'],
-      action: '{ $$ = [...$1, $2] }',
-    },
-    {lhs: 'Definition', rhs: ['ExecutableDefinition']},
-    {lhs: 'ExecutableDefinition', rhs: ['OperationDefinition']},
-    {lhs: 'OperationDefinition', rhs: ['SelectionSet']},
-    {
-      lhs: 'SelectionSet',
-      rhs: ['OPENING_BRACE', 'SelectionList', 'CLOSING_BRACE'],
-    },
-    {lhs: 'SelectionList', rhs: ['Selection']},
-    {
-      lhs: 'SelectionList',
-      rhs: ['SelectionList', 'Selection'],
-      action: '{ $$ = [...$1, $2] }',
-    },
-    {lhs: 'Selection', rhs: ['Field']},
-    {lhs: 'Field', rhs: ['NAME']},
-  ],
-};
-*/
+const grammarDeclaration = `
+  %token AMPERSAND
+  %token AT
+  %token BANG
+  %token BAR
+  %token COLON
+  %token DOLLAR
+  %token ELLIPSIS
+  %token EQUALS
+  %token NAME
+  %token NUMBER
+  %token OPENING_BRACE CLOSING_BRACE
+  %token OPENING_BRACKET CLOSING_BRACKET
+  %token OPENING_PAREN CLOSING_PAREN
+  %token STRING_VALUE BLOCK_STRING_VALUE
 
-/*
-export const grammarDeclaration = `
-    %token CLOSING_BRACE NAME OPENING_BRACE
-
-    Document → DefinitionList
-    DefinitionList → Definition
-    DefinitionList → DefinitionList Definition { $$ = [...$1, $2] }
-    Definition → ExecutableDefinition
-    ExecutableDefinition → OperationDefinition
-    OperationDefinition → SelectionSet
-    SelectionSet → OPENING_BRACE SelectionList CLOSING_BRACE
-    SelectionList → Selection
-    SelectionList → SelectionList Selection { $$ = [...$1, $2] }
-    Selection → Field
-    Field → NAME
+  Document → DefinitionList
+  DefinitionList → Definition { $$ = [$1]; }
+  DefinitionList → DefinitionList Definition { $1.push($2); $$ = $1; }
+  Definition → ExecutableDefinition
+  ExecutableDefinition → OperationDefinition
+  OperationDefinition → SelectionSet
+  SelectionSet → OPENING_BRACE SelectionList CLOSING_BRACE { $$ = $2; }
+  SelectionList → Selection { $$ = [$1]; }
+  SelectionList → SelectionList Selection { $1.push($2); $$ = $1; }
+  Selection → Field
+  Field → NAME
 `;
-*/
+
+export const grammar = parseDSL(grammarDeclaration);
+export const itemSets = getItemSets(grammar);
+export const transitionTable = itemSetsToTransitionTable(itemSets, grammar);
+export const table = getParseTable(itemSets, transitionTable, grammar);
