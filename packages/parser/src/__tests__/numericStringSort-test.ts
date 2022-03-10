7479fb9824d101ce29f6ec9039d184f0c7544751
Greg Hurrell <greg@hurrell.net>
2022-03-10 20:15:51 +0100

    feat(parser): add `numericStringSort()`
    But as noted inline, I am a clown. Next commit will delete most of
    this.

---
+++ packages/parser/src/__tests__/numericStringSort-test.ts
@@ -0,0 +1,54 @@
+import numericStringSort from '../numericStringSort';
+
+describe('numericStringSort()', () => {
+  it('sorts words', () => {
+    expect(numericStringSort(['foo', 'bar', 'baz', 'qux'])).toEqual(
+      ['bar', 'baz', 'foo', 'qux']);
+  });
+
+  it('sorts strings with substrings that start with numbers', () => {
+    expect(numericStringSort([
+      '%token 1/THING/2',
+      '%token 10/BOOM/4',
+      '%token 2/BOOP/3',
+    ])).toEqual([
+      '%token 1/THING/2',
+      '%token 2/BOOP/3',
+      '%token 10/BOOM/4',
+    ]);
+  });
+
+  it('sorts strings with substrings that contain numbers', () => {
+    expect(numericStringSort([
+      'r10 → C',
+      'r10 → ε',
+      'r100 → FOO r1',
+      'r1 → B',
+      'r1 → A',
+      'r2 → D',
+    ])).toEqual([
+      'r1 → A',
+      'r1 → B',
+      'r2 → D',
+      'r10 → C',
+      'r10 → ε',
+      'r100 → FOO r1',
+    ]);
+
+    expect(numericStringSort([
+      'r10 → 10/C/12',
+      'r10 → 10/ε/11',
+      'r1 → 1/B/2',
+      'r100 → FOO 100/r1/2',
+      'r1 → 1/A/3',
+      'r2 → 2/D/5',
+    ])).toEqual([
+      'r1 → 1/A/3',
+      'r1 → 1/B/2',
+      'r2 → 2/D/5',
+      'r10 → 10/C/12',
+      'r10 → 10/ε/11',
+      'r100 → FOO 100/r1/2',
+    ]);
+  });
+});
