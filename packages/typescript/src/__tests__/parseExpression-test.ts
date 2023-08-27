a94db8bade09479954c0617279bc263823ea4834
Greg Hurrell <greg@hurrell.net>
2023-08-27 11:00:22 +0200

    test(typescript): fix bad test
    Blinking light demo actually does work if you write the test right.

--- packages/typescript/src/__tests__/parseExpression-test.ts
+++ packages/typescript/src/__tests__/parseExpression-test.ts
@@ -4,8 +4,22 @@
 import parseExpression from '../parseExpression';
 
 describe('parseExpression()', () => {
-  it.failing('it parses a strict equality binary expression', () => {
-    const input = 'foo = 1.5';
-    expect(parseExpression(input)).toMatchInlineSnapshot();
+  it('it parses a strict equality binary expression', () => {
+    const input = 'foo === 1.5';
+    expect(parseExpression(input)).toMatchInlineSnapshot(`
+      {
+        "kind": "BinaryExpression",
+        "lexpr": {
+          "kind": "Identifier",
+          "name": "foo",
+        },
+        "operator": "===",
+        "rexpr": {
+          "base": 10,
+          "kind": "NumberValue",
+          "value": 1.5,
+        },
+      }
+    `);
   });
 });
