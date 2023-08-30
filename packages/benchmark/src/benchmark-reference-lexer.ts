83133f488290cda3a85ce33eed50c824d435f45a
Greg Hurrell <greg@hurrell.net>
2023-08-30 22:53:20 +0200

    refactor: simplify graphql dependency
    I didn't want to pull in any dependencies if I could help it, but this
    was really just sweeping it under the rug. graphql doesn't have any
    non-dev deps, so this is really fine, and cleaner overall to boot.

--- packages/benchmark/src/benchmark-reference-lexer.ts
+++ packages/benchmark/src/benchmark-reference-lexer.ts
@@ -1,49 +1,20 @@
-import {spawnSync} from 'child_process';
-import fs from 'fs';
-import os from 'os';
-import path from 'path';
-import {promisify} from 'util';
+import {Lexer, Source} from 'graphql';
 
 import run from './benchmark-lexer';
 
-const mkdtemp = promisify(fs.mkdtemp);
-
-async function main() {
-  const scratch = await mkdtemp(path.join(os.tmpdir(), 'bench-'));
-
-  process.chdir(scratch);
-
-  spawn('yarn', 'init', '-y');
-  spawn('yarn', 'add', 'graphql');
-
-  const {Lexer, Source} = require(
-    path.join(scratch, ['node', 'modules'].join('_'), 'graphql'),
-  );
-
-  function* lex(input: string) {
-    const lexer = new Lexer(new Source(input));
-    while (true) {
-      const token = lexer.advance();
-      if (token.kind === '<EOF>') {
-        break;
-      }
-      yield token;
+function* lex(input: string) {
+  const lexer = new Lexer(new Source(input));
+  while (true) {
+    const token = lexer.advance();
+    if (token.kind === '<EOF>') {
+      break;
     }
+    yield token;
   }
-
-  await run(lex);
 }
 
-function spawn(command: string, ...args: Array<string>) {
-  const {error, signal, status, stdout} = spawnSync(command, args, {
-    stdio: 'inherit',
-  });
-
-  if (status !== 0 || signal !== null || error) {
-    throw new Error('Something bad happened');
-  }
-
-  return stdout;
+async function main() {
+  await run(lex);
 }
 
 main().catch((error) => {
--- packages/benchmark/src/benchmark-reference-parser.ts
+++ packages/benchmark/src/benchmark-reference-parser.ts
@@ -1,39 +1,9 @@
-import {spawnSync} from 'child_process';
-import fs from 'fs';
-import os from 'os';
-import path from 'path';
+import {parse} from 'graphql';
 
 import run from './benchmark-parser';
 
-import {promisify} from 'util';
-
-const mkdtemp = promisify(fs.mkdtemp);
-
 async function main() {
-  const scratch = await mkdtemp(path.join(os.tmpdir(), 'bench-'));
-
-  process.chdir(scratch);
-
-  spawn('yarn', 'init', '-y');
-  spawn('yarn', 'add', 'graphql');
-
-  const {parse} = require(
-    path.join(scratch, ['node', 'modules'].join('_'), 'graphql'),
-  );
-
   await run(parse);
-}
-
-function spawn(command: string, ...args: Array<string>) {
-  const {error, signal, status, stdout} = spawnSync(command, args, {
-    stdio: 'inherit',
-  });
-
-  if (status !== 0 || signal !== null || error) {
-    throw new Error('Something bad happened');
-  }
-
-  return stdout;
 }
 
 main().catch((error) => {
