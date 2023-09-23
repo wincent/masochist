a0ae2569151d218e36ef1729ed7f984374d26f79
Greg Hurrell <greg@hurrell.net>
2023-09-23 20:59:58 +0200

    refactor: trim formatting scripts

--- support/check-format.sh
+++ support/check-format.sh
@@ -5,7 +5,7 @@ DIRECTORY=$(cd $(dirname "$BASH_SOURCE"); pwd)
 cd "$DIRECTORY/.."
 
 if [ $# -eq 0 ]; then
-	bun prettier --log-level warn --check '**/*.{graphql,js,json,md,mjs,ts,yml}'
+	bun prettier --log-level warn --check '**/*.{graphql,json,md,ts,yml}'
 else
 	bun prettier --log-level warn --check "$@"
 fi
--- support/format.sh
+++ support/format.sh
@@ -5,7 +5,7 @@ DIRECTORY=$(cd $(dirname "$BASH_SOURCE"); pwd)
 cd "$DIRECTORY/.."
 
 if [ $# -eq 0 ]; then
-	bun prettier --log-level warn --write '**/*.{graphql,js,json,md,mjs,ts,yml}'
+	bun prettier --log-level warn --write '**/*.{graphql,json,md,ts,yml}'
 else
 	bun prettier --log-level warn --write "$@"
 fi
